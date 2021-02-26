c26bef5b1b13623a238500d97909873a549cecd7
Qingyun Wu <qw2ky@virginia.edu>
2021-02-25 19:04:39 -0800

    add lograndint

--- flaml/tune/__init__.py
+++ flaml/tune/__init__.py
@@ -1,7 +1,7 @@
 try:
     from ray.tune import (uniform, quniform, choice, randint, qrandint, randn,
- qrandn, loguniform, qloguniform)
+ qrandn, loguniform, qloguniform, lograndint)
 except:
     from .sample import (uniform, quniform, choice, randint, qrandint, randn,
- qrandn, loguniform, qloguniform)
+ qrandn, loguniform, qloguniform, lograndint)
 from .tune import run, report
