268f83c67ccae147b1533a2a8748b22492743113
Qingyun Wu <qiw@microsoft.com>
2021-02-27 07:01:13 +0000

    remove lograndint

--- flaml/tune/__init__.py
+++ flaml/tune/__init__.py
@@ -1,7 +1,7 @@
 try:
     from ray.tune import (uniform, quniform, choice, randint, qrandint, randn,
- qrandn, loguniform, qloguniform, lograndint)
+ qrandn, loguniform, qloguniform)
 except:
     from .sample import (uniform, quniform, choice, randint, qrandint, randn,
- qrandn, loguniform, qloguniform, lograndint)
+ qrandn, loguniform, qloguniform)
 from .tune import run, report
