b6c9dce16b08fa1a701e7bc56ebd7dea387a09ed
Chi Wang (MSR) <chiw@microsoft.com>
2021-03-16 20:49:01 -0700

    delta time

--- flaml/automl.py
+++ flaml/automl.py
@@ -1229,8 +1229,7 @@ class AutoML:
                                  search_state.best_loss) or \
                                      search_state.best_loss
                     delta_time = (search_state.total_time_used - 
-                                 search_state.time_best_found_old) or (
-                                     search_state.total_time_used + 1e-10)
+                                 search_state.time_best_found_old) or 1e-10
                     speed = delta_loss / delta_time
                     try:
                         estimated_cost = 2*gap/speed
