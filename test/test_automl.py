c5c6fd212c22b24aadf758fc1a4f884505189e9e
Chi Wang (MSR) <chiw@microsoft.com>
2021-03-18 22:37:35 -0700

    test drop columns and small training data

--- test/test_automl.py
+++ test/test_automl.py
@@ -174,6 +174,7 @@ class TestAutoML(unittest.TestCase):
         X_train, y_train = load_iris(return_X_y=True, as_frame=as_frame)
         if as_frame:
             # test drop column
+            X_train.columns = range(X_train.shape[1])
             X_train[X_train.shape[1]] = np.zeros(len(y_train))
         automl_experiment.fit(X_train=X_train, y_train=y_train,
                               **automl_settings)
@@ -332,9 +333,10 @@ class TestAutoML(unittest.TestCase):
             "log_file_name": "test/sparse_regression.log",
             "n_jobs": 1,
             "model_history": True,
+            "metric": "mse"
         }
-        X_train = scipy.sparse.random(100, 100)
-        y_train = np.random.uniform(size=100)
+        X_train = scipy.sparse.random(8, 100)
+        y_train = np.random.uniform(size=8)
         automl_experiment.fit(X_train=X_train, y_train=y_train,
                               **automl_settings)
         print(automl_experiment.predict(X_train))
