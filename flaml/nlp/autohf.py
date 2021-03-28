2f1143a1744476b870d7506d6839adec03fa6a1f
liususan091219 <Xqq630517>
2021-03-27 21:04:11 -0400

    Merge branch 'squad' into v0.2.7

--- flaml/nlp/autohf.py
+++ flaml/nlp/autohf.py
@@ -245,6 +245,21 @@ class AutoHuggingFace:
                     "documentation of AutoHuggingFace.prepare_data()".format(",".join(fold_keys))
         return "train", "validation", "test"
 
+    def _tokenize(self,
+                  examples,
+                  sentence_keys):
+        if len(sentence_keys) > 1:
+            sentence1_key, sentence2_key = sentence_keys[0], sentence_keys[1]
+        else:
+            sentence1_key = sentence_keys[0]
+            sentence2_key = None
+
+        args = (
+            (examples[sentence1_key],) if sentence2_key is None else (
+                examples[sentence1_key], examples[sentence2_key])
+        )
+        return self._tokenizer(*args, padding="max_length", max_length=self._max_seq_length, truncation=True)
+
     def prepare_data(self,
                      dataset_config,
                      model_name,
