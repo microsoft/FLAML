fa2eda6952d9e64a58098e511fa4d3b52b39d418
liususan091219 <Xqq630517>
2021-03-28 01:09:32 -0400

    Merge branch 'squad' into v0.2.7

--- test/hf/run_autohf.py
+++ test/hf/run_autohf.py
@@ -42,7 +42,7 @@ def _test_electra():
                     "dataset_config": {"task": dataset_to_task_mapping[this_dataset_name[0]],
                                        "dataset_name": this_dataset_name,
                                        "subdataset_name": this_subset_name,
-                                       "fold_name": ['train', 'validation_matched', 'test_matched']
+                                       #"fold_name": ['train', 'validation_matched', 'test_matched']
                                     },
                     "model_name": each_pretrained_model,
                     "split_mode": "origin",
