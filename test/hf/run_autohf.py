b27810d87f93815a1221da584414ed3e4e3c296e
liususan091219 <Xqq630517>
2021-04-04 23:28:04 -0400

    fixed wandb problem, changed wandb callback -> wandb mixin; added trainer.evaluate to _objective

--- test/hf/run_autohf.py
+++ test/hf/run_autohf.py
@@ -30,7 +30,7 @@ search_algos = ["BlendSearch"]
 scheduler_names = ["None"]
 
 hpo_searchspace_modes = ["hpo_space_gridunion"]
-search_algo_args_modes = ["grid"]
+search_algo_args_modes = ["default"]
 num_sample_time_budget_mode, time_as_grid = ("times_grid_time_budget", 4.0)
 
 def get_full_name(autohf, is_grid, hpo_searchspace_mode = None):
