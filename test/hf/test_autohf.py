8864090c80be0f87174f646f8809121712b5b086
Chi Wang <wang.chi@microsoft.com>
2021-04-16 13:49:44 -0700

    test

--- test/hf/test_autohf.py
+++ test/hf/test_autohf.py
@@ -1,8 +1,8 @@
-'''Require: pip install torch transformers datasets flaml[blendsearch,ray]
+'''Require: pip install torch transformers datasets wandb flaml[blendsearch,ray]
 '''
-import ray
 import subprocess, platform
 from flaml.nlp.autotransformers import AutoTransformers
+
 
 def test_electra(method='BlendSearch'):
     # setting wandb key
@@ -18,15 +18,19 @@ def test_electra(method='BlendSearch'):
             "model_name": "google/mobilebert-uncased",
             "server_name": "tmdev",
             "split_mode": "resplit",
-            "resplit_portion": {"source": ["train", "validation"], "train": [0, 0.01], "validation": [0.01, 0.02], "test": [0.02, 0.03]},
-            "ckpt_path": "../../../data/checkpoint/",
-            "result_path": "../../../data/result/",
-            "log_path": "../../../data/result/",
+            "resplit_portion": {
+                "source": ["train", "validation"],
+                "train": [0, 0.01],
+                "validation": [0.01, 0.02],
+                "test": [0.02, 0.03]},
+            "ckpt_path": "data/checkpoint/",
+            "result_path": "data/result/",
+            "log_path": "data/result/",
             "max_seq_length": 128,
         }
 
-        train_dataset, eval_dataset, test_dataset =\
-            autohf.prepare_data(**preparedata_setting)
+        train_dataset, eval_dataset, test_dataset = autohf.prepare_data(
+            **preparedata_setting)
 
         autohf_settings = {"resources_per_trial": {"cpu": 1},
                            "wandb_key": wandb_key,
@@ -38,13 +42,14 @@ def test_electra(method='BlendSearch'):
                            "points_to_evaluate": [{
                                "num_train_epochs": 1,
                                "per_device_train_batch_size": 4, }]
-                           }
+                          }
 
         autohf.fit(train_dataset,
                    eval_dataset,
-                   **autohf_settings,)
+                   **autohf_settings)
 
         #predictions = autohf.predict(test_dataset)
+
 
 if __name__ == "__main__":
     test_electra()
