0699431d029284154216a2a0f259e2cdd7e139d3
Chi Wang <wang.chi@microsoft.com>
2021-04-17 21:45:15 -0700

    check wandb mode

--- flaml/nlp/autotransformers.py
+++ flaml/nlp/autotransformers.py
@@ -821,7 +821,8 @@ class AutoTransformers:
         self._set_sample_num_time_budget(custom_num_samples, custom_time_budget, num_sample_time_budget_mode, time_as_grid)
         scheduler = AutoScheduler.from_scheduler_name(self._scheduler_name)
 
-        self._set_wandb(wandb_key)
+        if os.environ.get("WANDB_MODE") != 'disabled':
+            self._set_wandb(wandb_key)
         self.path_utils.make_dir_per_run()
 
         logger.addHandler(logging.FileHandler(os.path.join(self.path_utils.log_dir_per_run, 'tune.log')))
