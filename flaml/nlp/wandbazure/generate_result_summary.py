bb2824858325a885ea52e483f24db461fb96ef81
liususan091219 <Xqq630517>
2021-05-06 10:41:37 -0400

    updating run_summary, adding row names + automatically generating array formulas

--- flaml/nlp/wandbazure/generate_result_summary.py
+++ flaml/nlp/wandbazure/generate_result_summary.py
@@ -6,16 +6,18 @@ import argparse
 from flaml.nlp.wandbazure.utils import extract_ts, compare_dates
 from flaml.nlp.wandbazure.utils import get_all_runs, init_blob_client
 
-all_tasks = ["glue_mrpc", "glue_rte", "glue_cola"]
-search_algos = ["BlendSearch", "BlendSearch", "Optuna", "RandomSearch", "CFO"]
-scheduler_names = ["None", "ASHA", "None", "ASHA", "None"]
+# all_tasks = ["glue_mrpc", "glue_rte", "glue_cola"]
+# search_algos = ["BlendSearch", "BlendSearch", "Optuna", "RandomSearch", "CFO"]
+# scheduler_names = ["None", "ASHA", "None", "ASHA", "None"]
+#
+# hpo_searchspace_modes = ["generic", "gridunion"]
 
-hpo_searchspace_modes = ["generic", "gridunion"]
-
-algo_space_to_summarize = [(4, 1)]
+algo_space_to_summarize = [(0, 1), (2, 1), (4, 1)]
 
 repid_max = 4
 modelid_max = 5
+
+COLUMN_OFFSET=ROW_OFFSET=1
 
 def remove_by_date(tasklist, earliest_ts):
     earliest_time = extract_ts(earliest_ts)
@@ -30,10 +32,14 @@ def remove_by_date(tasklist, earliest_ts):
         if compare_dates(this_time, earliest_time) == -1:
             each_file.delete()
 
-def generate_result_csv(args, task2blobs):
-    for task_id in range(len(all_tasks)):
-        all_blobs = task2blobs[all_tasks[task_id]]
+def generate_result_csv(args, task2blobs, dataset_names, subdataset_names, search_algos, pretrained_models, scheduler_names, hpo_searchspace_modes, search_algo_args_modes):
+    id2model = {}
+    for task_id in range(3): #len(dataset_names)):
+        full_name = dataset_names[task_id][0] + "_" + subdataset_names[task_id]
+        all_blobs = task2blobs[full_name]
         tasktab = None
+        arrayformulatab = None
+
         is_first = True
         new_columnnames = []
 
@@ -43,8 +49,9 @@ def generate_result_csv(args, task2blobs):
             new_columnnames = new_columnnames + [subtab_name, "", "", "", "", "", ""]
             offset = each_algo_space_id * 7
             each_df = pd.DataFrame(
-                index = [x for x in range(modelid_max * (repid_max + 1))],
+                index = [x for x in range((modelid_max) * (repid_max + 1))],
                 columns=[offset + x for x in range(5)])
+            each_arrayforula = pd.DataFrame(index=[0], columns=[offset + x for x in range(5)])
             for model_id in range(modelid_max):
                 for rep_id in range(repid_max):
                     insert_id = model_id * (repid_max + 1) + rep_id
@@ -62,14 +69,33 @@ def generate_result_csv(args, task2blobs):
                         for x in range(5):
                             each_df[offset + x][insert_id] = result[x]
 
+            arrayformula_indices = [offset + 3, offset + 4]
+            for each_index in arrayformula_indices:
+                first_part = int((each_index + COLUMN_OFFSET + 1) / 26)
+                second_part = (each_index + COLUMN_OFFSET + 1) % 26
+                first_char = "" if first_part == 0 else chr(first_part + 64)
+                second_char = chr(65 + second_part)
+                arrayformula_col = first_char + second_char
+                arrayformula_row1 = 2 + ROW_OFFSET
+                arrayformula_row2 = ROW_OFFSET + (modelid_max) * (repid_max + 1)
+                start_cell = arrayformula_col + str(arrayformula_row1)
+                end_cell = arrayformula_col + str(arrayformula_row2)
+                each_arrayforula[each_index] = "=average(" + start_cell + ":" + end_cell + ")"
+
             each_df = each_df.reindex(columns=each_df.columns.tolist()
                                     + [offset + 5,
                                        offset + 6])
+            each_arrayforula = each_arrayforula.reindex(columns = each_arrayforula.columns.tolist() + [offset + 5, offset + 6])
+
             if is_first:
                 tasktab = each_df
+                arrayformulatab = each_arrayforula
                 is_first = False
             else:
                 tasktab = pandas.concat([tasktab, each_df], axis=1)
+                arrayformulatab = pandas.concat([arrayformulatab, each_arrayforula], axis = 1)
 
+        tasktab.index = [pretrained_models[int (x / (repid_max + 1))][0] if x %(repid_max + 1) == 0 else "" for x in range(modelid_max * (repid_max + 1))]
         tasktab.columns = new_columnnames
-        tasktab.to_csv(all_tasks[task_id] + ".csv", index=False)
+        tasktab.to_csv(full_name + ".csv", index=True)
+        arrayformulatab.to_csv(full_name + "_arrayformula.csv", index = False)
--- test/hf/run_summary.py
+++ test/hf/run_summary.py
@@ -6,6 +6,7 @@ import argparse,json
 from flaml.nlp.wandbazure.generate_result_summary import generate_result_csv
 from flaml.nlp.wandbazure.utils import get_all_runs
 from utils import get_wandb_azure_key
+from run_autohf import dataset_names, subdataset_names, search_algos, pretrained_models, scheduler_names, hpo_searchspace_modes, search_algo_args_modes
 
 if __name__ == "__main__":
     arg_parser = argparse.ArgumentParser()
@@ -17,4 +18,4 @@ if __name__ == "__main__":
     wandb_key, args.azure_key = get_wandb_azure_key()
     task2blobs, tasklist = get_all_runs(args)
 
-    generate_result_csv(args, task2blobs)
+    generate_result_csv(args, task2blobs, dataset_names, subdataset_names, search_algos, pretrained_models, scheduler_names, hpo_searchspace_modes, search_algo_args_modes)
