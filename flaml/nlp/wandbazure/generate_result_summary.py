f18173ac0988fb0fb5c6924747690b6b33c01847
liususan091219 <Xqq630517>
2021-05-06 16:01:52 -0400

    updating run_summary, adding row names + automatically generating array formulas

--- flaml/nlp/wandbazure/generate_result_summary.py
+++ flaml/nlp/wandbazure/generate_result_summary.py
@@ -6,12 +6,6 @@ import argparse
 from flaml.nlp.wandbazure.utils import extract_ts, compare_dates
 from flaml.nlp.wandbazure.utils import get_all_runs, init_blob_client
 
-# all_tasks = ["glue_mrpc", "glue_rte", "glue_cola"]
-# search_algos = ["BlendSearch", "BlendSearch", "Optuna", "RandomSearch", "CFO"]
-# scheduler_names = ["None", "ASHA", "None", "ASHA", "None"]
-#
-# hpo_searchspace_modes = ["generic", "gridunion"]
-
 algo_space_to_summarize = [(0, 1), (2, 1), (4, 1)]
 
 repid_max = 4
@@ -32,70 +26,43 @@ def remove_by_date(tasklist, earliest_ts):
         if compare_dates(this_time, earliest_time) == -1:
             each_file.delete()
 
-def generate_result_csv(args, task2blobs, dataset_names, subdataset_names, search_algos, pretrained_models, scheduler_names, hpo_searchspace_modes, search_algo_args_modes):
-    id2model = {}
-    for task_id in range(3): #len(dataset_names)):
-        full_name = dataset_names[task_id][0] + "_" + subdataset_names[task_id]
-        all_blobs = task2blobs[full_name]
-        tasktab = None
-        arrayformulatab = None
+def generate_result_csv(args, bloblist, dataset_names, subdataset_names, search_algos, pretrained_models, scheduler_names, hpo_searchspace_modes, search_algo_args_modes):
 
-        is_first = True
-        new_columnnames = []
+    tab = pd.DataFrame(
+        columns=["full_dataset", "rep_id", "space", "wandb_hash", "model", "algorithm", "pruner", "sample_num", "time", "val_acc", "test_acc"]
+        , index=[x for x in range(len(bloblist))])
+    for blob_id in range(len(bloblist)):
+        blobname = bloblist[blob_id]
+        result = re.search(".*_model(?P<model_id>\d+)_(?P<algo_id>\d+)_(?P<space_id>\d+)_rep(?P<rep_id>\d+).log",
+                           blobname)
+        model_id = int(result.group("model_id"))
+        space_id = int(result.group("space_id"))
+        rep_id = int(result.group("rep_id"))
+        algo_id = int(result.group("algo_id"))
+        full_dataset = blobname.split("/")[1]
 
-        for each_algo_space_id in range(len(algo_space_to_summarize)):
-            algo_id, space_id = algo_space_to_summarize[each_algo_space_id]
-            subtab_name = search_algos[algo_id].lower() + "_" + scheduler_names[algo_id] + "_" + hpo_searchspace_modes[space_id]
-            new_columnnames = new_columnnames + [subtab_name, "", "", "", "", "", ""]
-            offset = each_algo_space_id * 7
-            each_df = pd.DataFrame(
-                index = [x for x in range((modelid_max) * (repid_max + 1))],
-                columns=[offset + x for x in range(5)])
-            each_arrayforula = pd.DataFrame(index=[0], columns=[offset + x for x in range(5)])
-            for model_id in range(modelid_max):
-                for rep_id in range(repid_max):
-                    insert_id = model_id * (repid_max + 1) + rep_id
-                    try:
-                        this_blob_file = all_blobs[model_id][algo_id][space_id][rep_id]
-                        blob_client = init_blob_client(args.azure_key, this_blob_file)
-                        pathlib.Path(re.search("(?P<parent_path>^.*)/[^/]+$", this_blob_file).group("parent_path")).mkdir(parents=True, exist_ok=True)
-                        with open(this_blob_file, "wb") as fout:
-                            fout.write(blob_client.download_blob().readall())
-                    except:
-                        break
-                    with open(this_blob_file, "r") as fin:
-                        alllines = fin.readlines()
-                        result = alllines[5].rstrip("\n").split(",")
-                        for x in range(5):
-                            each_df[offset + x][insert_id] = result[x]
+        try:
+            blob_client = init_blob_client(args.azure_key, blobname)
+            pathlib.Path(re.search("(?P<parent_path>^.*)/[^/]+$", blobname).group("parent_path")).mkdir(
+                parents=True, exist_ok=True)
+            with open(blobname, "wb") as fout:
+                fout.write(blob_client.download_blob().readall())
+        except:
+            break
+        with open(blobname, "r") as fin:
+            alllines = fin.readlines()
 
-            arrayformula_indices = [offset + 3, offset + 4]
-            for each_index in arrayformula_indices:
-                first_part = int((each_index + COLUMN_OFFSET + 1) / 26)
-                second_part = (each_index + COLUMN_OFFSET + 1) % 26
-                first_char = "" if first_part == 0 else chr(first_part + 64)
-                second_char = chr(65 + second_part)
-                arrayformula_col = first_char + second_char
-                arrayformula_row1 = 2 + ROW_OFFSET
-                arrayformula_row2 = ROW_OFFSET + (modelid_max) * (repid_max + 1)
-                start_cell = arrayformula_col + str(arrayformula_row1)
-                end_cell = arrayformula_col + str(arrayformula_row2)
-                each_arrayforula[each_index] = "=average(" + start_cell + ":" + end_cell + ")"
+            space = hpo_searchspace_modes[space_id]
+            if len(alllines) < 5: continue
+            wandb_hash = alllines[0].rstrip("\n:").split("_")[-1]
+            model = pretrained_models[model_id][0]
+            algo = search_algos[algo_id]
+            pruner = scheduler_names[algo_id]
+            sample_num = int(alllines[5].rstrip("\n").split(",")[1])
+            time = float(alllines[5].rstrip("\n").split(",")[2])
+            val_acc = float(alllines[5].rstrip("\n").split(",")[3])
+            test_acc = float(alllines[5].rstrip("\n").split(",")[4])
 
-            each_df = each_df.reindex(columns=each_df.columns.tolist()
-                                    + [offset + 5,
-                                       offset + 6])
-            each_arrayforula = each_arrayforula.reindex(columns = each_arrayforula.columns.tolist() + [offset + 5, offset + 6])
+            tab.loc[blob_id] = [full_dataset, rep_id, space, wandb_hash, model, algo, pruner, sample_num, time, val_acc, test_acc]
 
-            if is_first:
-                tasktab = each_df
-                arrayformulatab = each_arrayforula
-                is_first = False
-            else:
-                tasktab = pandas.concat([tasktab, each_df], axis=1)
-                arrayformulatab = pandas.concat([arrayformulatab, each_arrayforula], axis = 1)
-
-        tasktab.index = [pretrained_models[int (x / (repid_max + 1))][0] if x %(repid_max + 1) == 0 else "" for x in range(modelid_max * (repid_max + 1))]
-        tasktab.columns = new_columnnames
-        tasktab.to_csv(full_name + ".csv", index=True)
-        arrayformulatab.to_csv(full_name + "_arrayformula.csv", index = False)
+    tab.to_csv("result.csv", index = False)
--- test/hf/run_summary.py
+++ test/hf/run_summary.py
@@ -16,6 +16,6 @@ if __name__ == "__main__":
     args = arg_parser.parse_args()
 
     wandb_key, args.azure_key = get_wandb_azure_key()
-    task2blobs, tasklist = get_all_runs(args)
+    task2blobs, bloblist = get_all_runs(args)
 
-    generate_result_csv(args, task2blobs, dataset_names, subdataset_names, search_algos, pretrained_models, scheduler_names, hpo_searchspace_modes, search_algo_args_modes)
+    generate_result_csv(args, bloblist, dataset_names, subdataset_names, search_algos, pretrained_models, scheduler_names, hpo_searchspace_modes, search_algo_args_modes)
