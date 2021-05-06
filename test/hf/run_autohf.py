049770178cff6dac7a2bbcb74f500c2d10b46980
liususan091219 <Xqq630517>
2021-05-06 00:21:12 -0400

    updating generate_result_summary.py

--- test/hf/run_autohf.py
+++ test/hf/run_autohf.py
@@ -8,7 +8,7 @@ import pathlib
 import shutil
 from flaml.nlp.autotransformers import AutoTransformers
 from flaml.nlp.wandbazure.utils import flush_and_upload, clean_outdated_results
-from test.hf.utils import get_wandb_azure_key
+from utils import get_wandb_azure_key
 
 global azure_log_path
 global azure_key
