c774d08a930fa2b00d54b5e903a07fd83d0241e4
liususan091219 <Xqq630517>
2021-05-07 18:53:14 -0400

    adding ASHA

--- flaml/nlp/hpo/hpo_searchspace.py
+++ flaml/nlp/hpo/hpo_searchspace.py
@@ -121,7 +121,7 @@ def hpo_space_generic(logger, model_type, model_size_type, dataset_name, subdata
     output_config = {
         "learning_rate": {"l": 1e-6, "u": 1e-3, "space": "log"},
         "num_train_epochs": {"l": 1.0, "u": 10.0, "space": "log"},
-        "per_device_train_batch_size": [4, 8, 16, 32, 48, 64],
+        "per_device_train_batch_size": [4, 8, 16, 32, 48],
         "warmup_ratio": {"l": 0.0, "u": 0.3, "space": "linear"},
         "weight_decay": {"l": 0.0, "u": 0.3, "space": "linear"}
     }
