1a3c17c3b95b89482d265f8bf67d7596234b0677
liususan091219 <Xqq630517>
2021-05-09 12:51:25 -0400

    adding init_config to generate_result_summary.py

--- flaml/nlp/wandbazure/generate_result_summary.py
+++ flaml/nlp/wandbazure/generate_result_summary.py
@@ -29,7 +29,7 @@ def remove_by_date(tasklist, earliest_ts):
 def generate_result_csv(args, bloblist, dataset_names, subdataset_names, search_algos, pretrained_models, scheduler_names, hpo_searchspace_modes, search_algo_args_modes):
 
     tab = pd.DataFrame(
-        columns=["full_dataset", "rep_id", "space", "init_config", "wandb_hash", "model", "algorithm", "pruner", "sample_num", "time", "val_acc", "test_acc"]
+        columns=["full_dataset", "rep_id", "space", "init_config", "wandb_hash", "model", "model_size", "algorithm", "pruner", "sample_num", "time", "val_acc", "test_acc"]
         , index=[x for x in range(len(bloblist))])
     for blob_id in range(len(bloblist)):
         blobname = bloblist[blob_id]
@@ -57,6 +57,7 @@ def generate_result_csv(args, bloblist, dataset_names, subdataset_names, search_
             if len(alllines) < 5: continue
             wandb_hash = alllines[0].rstrip("\n:").split("_")[-1]
             model = pretrained_models[model_id][0]
+            model_size = pretrained_models[model_id][1]
             algo = search_algos[algo_id]
             pruner = scheduler_names[algo_id]
             sample_num = int(alllines[5].rstrip("\n").split(",")[1])
@@ -64,6 +65,6 @@ def generate_result_csv(args, bloblist, dataset_names, subdataset_names, search_
             val_acc = float(alllines[5].rstrip("\n").split(",")[3])
             test_acc = float(alllines[5].rstrip("\n").split(",")[4])
 
-            tab.loc[blob_id] = [full_dataset, rep_id, space, init_config, wandb_hash, model, algo, pruner, sample_num, time, val_acc, test_acc]
+            tab.loc[blob_id] = [full_dataset, rep_id, space, init_config, wandb_hash, model, model_size, algo, pruner, sample_num, time, val_acc, test_acc]
 
     tab.to_csv("result.csv", index = False)
