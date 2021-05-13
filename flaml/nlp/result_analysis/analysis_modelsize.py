da5ee0d2a48e713800a1c81fe5724fceb781a002
liususan091219 <Xqq630517>
2021-05-12 22:29:33 -0400

    adding funnel models of 4 sizes: 4x4x4, 6x6x6, 8x8x8, 10x10x10

--- flaml/nlp/result_analysis/analysis_modelsize.py
+++ flaml/nlp/result_analysis/analysis_modelsize.py
@@ -14,13 +14,10 @@ def get_config_to_score(dataset_name, subdataset_name, this_group_name):
     config2score = []
     for idx in range(0, len(runs)):
         run = runs[idx]
-        this_hp_config_str = "_".join([str(key) + "=" + str(run.summary[key])
-            for key in sorted(run.summary.keys())
-            if key in ("weight_decay", "learning_rate", "num_train_epochs", "per_device_train_batch_size", "warmup_ratio")])
         default_metric, _, _, _ = get_default_and_alternative_metric(dataset_name, subdataset_name)
         try:
             this_eval_acc = run.summary['eval/' + default_metric]
-            config2score.append((this_hp_config_str, this_eval_acc, run.name))
+            config2score.append((run.summary, default_metric, this_eval_acc, run.name))
         except KeyError:
             pass
     return config2score
@@ -38,43 +35,58 @@ def analysis_model_size(args, task2blobs, dataset_names, subdataset_names, searc
     subprocess.run(["wandb", "login", "--relogin", args.wandb_key])
     small_model_id = 3
     large_model_id = 5
+    model_type = "deberta"
 
     space_id = 1
-    algo_id = 2
+    algo_ids = [0, 2, 4]
 
     resplit_id = 0
 
-    for run_idx in range(len(dataset_names)):
+    key_list = ["weight_decay", "learning_rate", "num_train_epochs", "per_device_train_batch_size", "warmup_ratio"]
+    tab = pandas.DataFrame(columns= key_list + ["eval_score", "dataset", "model", "rep_id"])
+    modelsize2id = {"small": small_model_id, "large": large_model_id}
+
+    for run_idx in range(3): #len(dataset_names)):
         all_runs = []
         task_name = dataset_names[run_idx][0] + "_" + subdataset_names[run_idx]
         all_blobs = task2blobs[resplit_id][task_name]
-        configs = []
-        configs2scores = []
-        for model_id in [small_model_id, large_model_id]:
+        configs_str_list = []
+        configstr2scores = []
+        for model_size in ["small", "large"]:
+            model_id = modelsize2id[model_size]
             config2scores = {}
-            for rep_id in range(3):
-                this_blob_file = all_blobs[model_id][algo_id][space_id][rep_id]
-                blob_client = init_blob_client(args.azure_key, this_blob_file)
-                pathlib.Path(re.search("(?P<parent_path>^.*)/[^/]+$", this_blob_file).group("parent_path")).mkdir(
-                    parents=True, exist_ok=True)
-                with open(this_blob_file, "wb") as fout:
-                    fout.write(blob_client.download_blob().readall())
-                with open(this_blob_file, "r") as fin:
-                    this_group_name = fin.readline().rstrip(":\n")
-                    for (config, score, trial_name) in get_config_to_score(dataset_names[run_idx][0], subdataset_names[run_idx], this_group_name):
-                        config2scores.setdefault(config, [])
-                        config2scores[config].append(score)
-            configs.append(sorted(config2scores.keys(), key = lambda x: np.mean(config2scores[x]), reverse= True))
-            configs2scores.append(config2scores)
-        intersection_config = set(configs[0]).intersection(set(configs[1]))
-        list1 = [(x, str(np.mean(configs2scores[0][x]))) for x in configs[0] if x in intersection_config]
-        list2 = [(x, str(np.mean(configs2scores[1][x]))) for x in configs[1] if x in intersection_config]
-
-        pd1 = convert_list_to_pd_frame(list1)
-        pd2 = convert_list_to_pd_frame(list2)
-
-        pd1.to_csv("small_model.csv")
-        pd2.to_csv("large_model.csv")
-
-        similarity = kendalltau(list1, list2)
+            for algo_id in algo_ids:
+                for rep_id in range(3):
+                    this_blob_file = all_blobs[model_id][algo_id][space_id][rep_id]
+                    blob_client = init_blob_client(args.azure_key, this_blob_file)
+                    pathlib.Path(re.search("(?P<parent_path>^.*)/[^/]+$", this_blob_file).group("parent_path")).mkdir(
+                        parents=True, exist_ok=True)
+                    with open(this_blob_file, "wb") as fout:
+                        fout.write(blob_client.download_blob().readall())
+                    with open(this_blob_file, "r") as fin:
+                        this_group_name = fin.readline().rstrip(":\n")
+                        for (configs, default_metric, score, trial_name) in get_config_to_score(dataset_names[run_idx][0], subdataset_names[run_idx], this_group_name):
+                            this_hp_configs = [str(key) + "=" + str(configs[key]) for key in sorted(configs.keys()) if key in key_list]
+                            config2scores.setdefault("_".join(this_hp_configs), [])
+                            config2scores["_".join(this_hp_configs)].append(score)
+                            this_row = {key: configs[key] for key in key_list}
+                            this_row["eval_score"] = score
+                            this_row["dataset"] = task_name
+                            this_row["model"] = model_size
+                            this_row["rep_id"] = rep_id
+                            tab = tab.append(this_row, ignore_index=True)
+            configs_str_list.append(sorted(config2scores.keys(), key = lambda x: np.mean(config2scores[x]), reverse= True))
+            configstr2scores.append(config2scores)
+        # intersection_config = set(configs[0]).intersection(set(configs[1]))
+        # list1 = [(x, str(np.mean(configstr2scores[0][x]))) for x in configs_str_list[0] if x in intersection_config]
+        # list2 = [(x, str(np.mean(configstr2scores[1][x]))) for x in configs_str_list[1] if x in intersection_config]
+        #
+        # pd1 = convert_list_to_pd_frame(list1)
+        # pd2 = convert_list_to_pd_frame(list2)
+        #
+        # pd1.to_csv("small_model.csv")
+        # pd2.to_csv("large_model.csv")
+        #similarity = kendalltau(list1, list2)
         stop = 0
+
+    tab.to_csv(model_type + "_smallvlarge.csv")
