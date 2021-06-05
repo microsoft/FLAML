1e45ed5df157d42f826f294ef2d0355c68b816bd
Chi Wang (MSR) <chiw@microsoft.com>
2021-06-05 11:23:29 -0700

    position of import

--- flaml/onlineml/trial.py
+++ flaml/onlineml/trial.py
@@ -297,6 +297,11 @@ class VowpalWabbitTrial(BaseOnlineTrial):
             trial_id (str): id of the trial (if None, it will be generated in the constructor)
 
         """
+        try:
+            from vowpalwabbit import pyvw
+        except ImportError:
+            raise ImportError(
+                'To use AutoVW, please run pip install flaml[vw] to install vowpalwabbit')
         # attributes
         self.trial_id = self._config_to_id(config) if trial_id is None else trial_id
         logger.info('Create trial with trial_id: %s', self.trial_id)
@@ -304,11 +309,6 @@ class VowpalWabbitTrial(BaseOnlineTrial):
                          custom_trial_name, self.trial_id)
         self.model = None   # model is None until the config is scheduled to run
         self.result = None
-        try:
-            from vowpalwabbit import pyvw
-        except ImportError:
-            raise ImportError(
-                'To use AutoVW, please run pip install flaml[vw] to install vowpalwabbit')
         self.trainable_class = pyvw.vw
         # variables that are needed during online training
         self._metric = metric
