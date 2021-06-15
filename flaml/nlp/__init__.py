8d30845298d1e115e50b5c03320c6e9e0eef95f9
liususan091219 <Xqq630517>
2021-06-15 10:38:52 -0400

    addressed comments

--- flaml/nlp/__init__.py
+++ flaml/nlp/__init__.py
@@ -1,3 +1,3 @@
-from .hpo.hpo_searchspace import AutoHPOSearchSpace
 from .autotransformers import AutoTransformers
 from .result_analysis.azure_utils import AzureUtils, JobID
+from .result_analysis.wandb_utils import WandbUtils
