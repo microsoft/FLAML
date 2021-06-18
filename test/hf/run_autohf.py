76d95a939e1840608fa273bbce23991500bbd1a8
liususan091219 <Xqq630517>
2021-06-17 20:51:37 -0400

    updating test_hpo

--- test/hf/run_autohf.py
+++ test/hf/run_autohf.py
@@ -277,8 +277,8 @@ def _exhaustive_sweep(args,
         jobid_config.subdat
     )
 
-    gridunion_space["learning_rate"] = [args.varying_arg1]
-    gridunion_space["weight_decay"] = [args.varying_arg2]
+    gridunion_space["learning_rate"] = [args.learning_rate]
+    gridunion_space["weight_decay"] = [args.weight_decay]
     _test_hpo(args, jobid_config, autohf, wandb_utils, azure_utils,
               autohf_settings,
               root_log_path=args.root_log_path,
