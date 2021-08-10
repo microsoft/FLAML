02c6a39ad2379ea17260573d129028d8a8761b09
liususan091219 <Xqq630517>
2021-08-09 23:59:22 -0400

    try new learning rate

--- test/hf/run_analysis.py
+++ test/hf/run_analysis.py
@@ -463,8 +463,8 @@ def compare_muppet(console_args):
     from flaml.nlp.result_analysis.azure_utils import JobID, ConfigScoreList
     from flaml.nlp import AzureUtils
 
-    dats =[ "yelp-polarity"] #  ["imdb", "amazon-polarity", "glue",
-    subdats = [None, None, "sst2", None]
+    dats =[ "glue"] #  ["imdb", "amazon-polarity", "glue",
+    subdats = ["sst2"] #, None]
     for idx in range(len(dats)):
         partial_jobid_config = JobID()
         partial_jobid_config.dat = [dats[idx]]
