059e2001794903b2a8f322040464d8eef8582ff9
张少坤 <zhangshaokun@fuzhi.ai>
2021-12-09 01:44:51 +0800

    add searchBar

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -118,4 +118,20 @@ module.exports = {
         crossorigin: "anonymous",
     },
   ],  
+  plugins: [
+    // ... Your other plugins.
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      {
+        // ... Your options.
+        // `hashed` is recommended as long-term-cache of index file is possible.
+        hashed: true,
+        // For Docs using Chinese, The `language` is recommended to set to:
+        // ```
+        // language: ["en", "zh"],
+        // ```
+        // When applying `zh` in language, please install `nodejieba` in your project.
+      },
+    ],
+  ],
 };
