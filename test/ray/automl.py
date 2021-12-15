bad7e324dc45f0293c2920271c632a6c5d946e0a
Chi Wang <wang.chi@microsoft.com>
2021-12-15 22:04:47 +0000

    remove redis pwd

--- test/ray/automl.py
+++ test/ray/automl.py
@@ -5,7 +5,7 @@ def _test_ray_classification():
     from sklearn.datasets import make_classification
     import ray
 
-    ray.init(address="auto", _redis_password="NOPASS")
+    ray.init(address="auto")
     X, y = make_classification(1000, 10)
     automl = AutoML()
     automl.fit(X, y, time_budget=10, task="classification", n_concurrent_trials=2)
