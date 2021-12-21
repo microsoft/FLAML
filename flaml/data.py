9faeed0983038e29bf678be3c7054d1722f3b3b5
oberonbot <oberonbot@gmail.com>
2021-12-20 23:37:08 -0500

    Already update automl.py, data.py, ml.py, model.py and trainer.py

--- flaml/data.py
+++ flaml/data.py
@@ -25,14 +25,14 @@ FORECAST = "forecast"
 SUMMARIZATION = "summarization"
 NLG_TASKS = (SUMMARIZATION,)
 NLU_TASKS = (
-    MULTICHOICECLASSIFICATION,
     SEQREGRESSION,
     SEQCLASSIFICATION,
+    MULTICHOICECLASSIFICATION,
 )
 
 
 def _is_nlp_task(task):
-    if task in NLU_TASKS + NLG_TASKS:
+    if task in NLU_TASKS or task in NLG_TASKS:
         return True
     else:
         return False
@@ -349,7 +349,11 @@ class DataTransformer:
             )
             self._drop = drop
 
-        if task in CLASSIFICATION or not pd.api.types.is_numeric_dtype(y):
+        if (
+                task in CLASSIFICATION
+                or not pd.api.types.is_numeric_dtype(y)
+                and task not in NLG_TASKS
+        ):
             from sklearn.preprocessing import LabelEncoder
 
             self.label_transformer = LabelEncoder()
