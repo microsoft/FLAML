63d0863a1a2d144865b829b72db7d5d8b634d9db
liususan091219 <xliu127@stevens.edu>
2021-12-23 18:13:20 -0500

    merging

--- test/nlp/test_autohf_custom_metric_maxiter1.py
+++ test/nlp/test_autohf_custom_metric_maxiter1.py
@@ -23,7 +23,7 @@ def toy_metric(
 
 
 @pytest.mark.skipif(sys.platform == "darwin", reason="do not run on mac os")
-def test_max_iter_1():
+def test_custom_metric_and_maxiter1():
     from flaml import AutoML
     import requests
     from datasets import load_dataset
@@ -46,6 +46,8 @@ def test_max_iter_1():
     y_val = dev_dataset[label_key]
 
     automl = AutoML()
+
+    # testing when max_iter=1 and do retrain only without hpo
 
     automl_settings = {
         "gpu_per_trial": 0,
@@ -66,51 +68,12 @@ def test_max_iter_1():
     automl.fit(
         X_train=X_train, y_train=y_train, X_val=X_val, y_val=y_val, **automl_settings
     )
-    del automl
 
+    # testing calling custom metric in TransformersEstimator._compute_metrics_by_dataset_name
 
-@pytest.mark.skipif(sys.platform == "darwin", reason="do not run on mac os")
-def test_custom_metric():
-    from flaml import AutoML
-    import requests
-    from datasets import load_dataset
-
-    try:
-        train_dataset = (
-            load_dataset("glue", "mrpc", split="train").to_pandas().iloc[0:4]
-        )
-        dev_dataset = load_dataset("glue", "mrpc", split="train").to_pandas().iloc[0:4]
-    except requests.exceptions.ConnectionError:
-        return
-
-    custom_sent_keys = ["sentence1", "sentence2"]
-    label_key = "label"
-
-    X_train = train_dataset[custom_sent_keys]
-    y_train = train_dataset[label_key]
-
-    X_val = dev_dataset[custom_sent_keys]
-    y_val = dev_dataset[label_key]
-
-    automl = AutoML()
-
-    automl_settings = {
-        "gpu_per_trial": 0,
-        "max_iter": 3,
-        "time_budget": 5,
-        "task": "seq-classification",
-        "metric": toy_metric,
-        "log_file_name": "seqclass.log",
-    }
-
-    automl_settings["custom_hpo_args"] = {
-        "model_path": "google/electra-small-discriminator",
-        "output_dir": "data/output/",
-        "ckpt_per_epoch": 5,
-        "fp16": False,
-    }
-
+    automl_settings["max_iter"] = 3
     automl.fit(
         X_train=X_train, y_train=y_train, X_val=X_val, y_val=y_val, **automl_settings
     )
+
     del automl
