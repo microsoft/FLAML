d5f4327025b3b4718bbae11a7fa60ac22d46362d
liususan091219 <xliu127@stevens.edu>
2022-01-12 18:33:04 -0500

    add support to multiple checkpoints

--- test/nlp/test_autohf_custom_metric.py
+++ test/nlp/test_autohf_custom_metric.py
@@ -17,6 +17,7 @@ def custom_metric(
 ):
     from datasets import Dataset
     from flaml.model import TransformersEstimator
+    from flaml.nlp.utils import load_default_huggingface_metric_for_task
 
     if estimator._trainer is None:
         estimator._init_model_for_predict(X_test)
@@ -36,6 +37,97 @@ def custom_metric(
 
     metrics = trainer.evaluate(eval_dataset)
     trainer.compute_metrics = trainer_compute_metrics_cache
+
+    # import json
+    # json.dump({"key": str(estimator._metric)},
+    #           open("/data/xliu127/projects/hyperopt/FLAML/test/nlp/test.json", "w"))
+    auto_metric = load_default_huggingface_metric_for_task(estimator._task)
+
+    estimator_metric_cache = estimator._metric
+    estimator._metric = auto_metric
+
+    eval_dataloader = trainer.get_eval_dataloader(eval_dataset)
+    from transformers.trainer_pt_utils import (
+        nested_concat,
+        nested_numpify,
+        nested_truncate,
+    )
+
+    preds_host = None
+    labels_host = None
+    all_preds = None
+    all_labels = None
+
+    for step, inputs in enumerate(eval_dataloader):
+        loss, logits, labels = trainer.prediction_step(
+            trainer.model, inputs, prediction_loss_only=None, ignore_keys=None
+        )
+
+        if logits is not None:
+            logits = trainer._pad_across_processes(logits)
+            logits = trainer._nested_gather(logits)
+            preds_host = (
+                logits
+                if preds_host is None
+                else nested_concat(preds_host, logits, padding_index=-100)
+            )
+        if labels is not None:
+            labels = trainer._pad_across_processes(labels)
+            labels = trainer._nested_gather(labels)
+            labels_host = (
+                labels
+                if labels_host is None
+                else nested_concat(labels_host, labels, padding_index=-100)
+            )
+
+        # Gather all tensors and put them back on the CPU if we have done enough accumulation steps.
+        num_samples = len(eval_dataset)
+
+        if (
+            trainer.args.eval_accumulation_steps is not None
+            and (step + 1) % trainer.args.eval_accumulation_steps == 0
+        ):
+            if preds_host is not None:
+                logits = nested_numpify(preds_host)
+                all_preds = (
+                    logits
+                    if all_preds is None
+                    else nested_concat(all_preds, logits, padding_index=-100)
+                )
+            if labels_host is not None:
+                labels = nested_numpify(labels_host)
+                all_labels = (
+                    labels
+                    if all_labels is None
+                    else nested_concat(all_labels, labels, padding_index=-100)
+                )
+            preds_host, labels_host = None, None
+
+        if preds_host is not None:
+            logits = nested_numpify(preds_host)
+            all_preds = (
+                logits
+                if all_preds is None
+                else nested_concat(all_preds, logits, padding_index=-100)
+            )
+        if labels_host is not None:
+            labels = nested_numpify(labels_host)
+            all_labels = (
+                labels
+                if all_labels is None
+                else nested_concat(all_labels, labels, padding_index=-100)
+            )
+
+        if all_preds is not None:
+            all_preds = nested_truncate(all_preds, num_samples)
+        if all_labels is not None:
+            all_labels = nested_truncate(all_labels, num_samples)
+
+    eval_pred = all_preds, all_labels
+    val_loss_dict = estimator._compute_metrics_by_dataset_name(eval_pred)
+    metrics[auto_metric] = val_loss_dict["val_loss"]
+
+    estimator._metric = estimator_metric_cache
     return metrics["eval_loss"], metrics
 
 
