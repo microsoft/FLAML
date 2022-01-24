6c219baa909864b849cca9b0c069cbc08a8603a0
liususan091219 <xliu127@stevens.edu>
2022-01-24 16:20:38 -0500

    getting ride of hf dataset

--- test/nlp/test_autohf_classificationhead.py
+++ test/nlp/test_autohf_classificationhead.py
@@ -1,33 +1,28 @@
 def test_classification_head():
     from flaml import AutoML
-    import requests
     import pandas as pd
 
-    try:
-        train_data = {
-            "text": [
-                "i didnt feel humiliated",
-                "i can go from feeling so hopeless to so damned hopeful just from being around someone who cares and is awake",
-                "im grabbing a minute to post i feel greedy wrong",
-                "i am ever feeling nostalgic about the fireplace i will know that it is still on the property",
-            ],
-            "label": [0, 0, 3, 2],
-        }
-        train_dataset = pd.DataFrame(train_data)
+    train_data = {
+        "text": [
+            "i didnt feel humiliated",
+            "i can go from feeling so hopeless to so damned hopeful just from being around someone who cares and is awake",
+            "im grabbing a minute to post i feel greedy wrong",
+            "i am ever feeling nostalgic about the fireplace i will know that it is still on the property",
+        ],
+        "label": [0, 0, 3, 2],
+    }
+    train_dataset = pd.DataFrame(train_data)
 
-        dev_data = {
-            "text": [
-                "i am feeling grouchy",
-                "ive been feeling a little burdened lately wasnt sure why that was",
-                "ive been taking or milligrams or times recommended amount and ive fallen asleep a lot faster but i also feel like so funny",
-                "i feel as confused about life as a teenager or as jaded as a year old man",
-            ],
-            "label": [3, 0, 5, 4],
-        }
-        dev_dataset = pd.DataFrame(dev_data)
-
-    except requests.exceptions.ConnectionError:
-        return
+    dev_data = {
+        "text": [
+            "i am feeling grouchy",
+            "ive been feeling a little burdened lately wasnt sure why that was",
+            "ive been taking or milligrams or times recommended amount and ive fallen asleep a lot faster but i also feel like so funny",
+            "i feel as confused about life as a teenager or as jaded as a year old man",
+        ],
+        "label": [3, 0, 5, 4],
+    }
+    dev_dataset = pd.DataFrame(dev_data)
 
     custom_sent_keys = ["text"]
     label_key = "label"
