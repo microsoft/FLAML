1ecf5f5ded07478e01b5a05287083c31ff79dd45
Chi Wang <wang.chi@microsoft.com>
2022-01-30 02:00:40 +0000

    make lgbm tuning example a test

--- test/tune_example.py
+++ test/tune_example.py
@@ -23,6 +23,7 @@ def train_lgbm(config: dict) -> dict:
     params = LGBMEstimator(**config).params
     # train the model
     # train_set = lightgbm.Dataset(X_train, y_train)
+    # LightGBM only accepts the csv with valid number format, if even these string columns are set to ignore.
     train_set = lightgbm.Dataset(
         csv_file_name, params={"label_column": "name:MedHouseVal", "header": True}
     )
@@ -34,35 +35,40 @@ def train_lgbm(config: dict) -> dict:
     return {"mse": mse}
 
 
-# load a built-in search space from flaml
-flaml_lgbm_search_space = LGBMEstimator.search_space(X_train.shape)
-# specify the search space as a dict from hp name to domain; you can define your own search space same way
-config_search_space = {
-    hp: space["domain"] for hp, space in flaml_lgbm_search_space.items()
-}
-# give guidance about hp values corresponding to low training cost, i.e., {"n_estimators": 4, "num_leaves": 4}
-low_cost_partial_config = {
-    hp: space["low_cost_init_value"]
-    for hp, space in flaml_lgbm_search_space.items()
-    if "low_cost_init_value" in space
-}
-# initial points to evaluate
-points_to_evaluate = [
-    {
-        hp: space["init_value"]
-        for hp, space in flaml_lgbm_search_space.items()
-        if "init_value" in space
+def test_tune_lgbm_csv():
+    # load a built-in search space from flaml
+    flaml_lgbm_search_space = LGBMEstimator.search_space(X_train.shape)
+    # specify the search space as a dict from hp name to domain; you can define your own search space same way
+    config_search_space = {
+        hp: space["domain"] for hp, space in flaml_lgbm_search_space.items()
     }
-]
-# run the tuning, minimizing mse, with total time budget 3 seconds
-analysis = tune.run(
-    train_lgbm,
-    metric="mse",
-    mode="min",
-    config=config_search_space,
-    low_cost_partial_config=low_cost_partial_config,
-    points_to_evaluate=points_to_evaluate,
-    time_budget_s=3,
-    num_samples=-1,
-)
-print(analysis.best_result)
+    # give guidance about hp values corresponding to low training cost, i.e., {"n_estimators": 4, "num_leaves": 4}
+    low_cost_partial_config = {
+        hp: space["low_cost_init_value"]
+        for hp, space in flaml_lgbm_search_space.items()
+        if "low_cost_init_value" in space
+    }
+    # initial points to evaluate
+    points_to_evaluate = [
+        {
+            hp: space["init_value"]
+            for hp, space in flaml_lgbm_search_space.items()
+            if "init_value" in space
+        }
+    ]
+    # run the tuning, minimizing mse, with total time budget 3 seconds
+    analysis = tune.run(
+        train_lgbm,
+        metric="mse",
+        mode="min",
+        config=config_search_space,
+        low_cost_partial_config=low_cost_partial_config,
+        points_to_evaluate=points_to_evaluate,
+        time_budget_s=3,
+        num_samples=-1,
+    )
+    print(analysis.best_result)
+
+
+if __name__ == "__main__":
+    test_tune_lgbm_csv()
