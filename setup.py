8f3d6735601b5cbeadd199b3a8b84e9c6819e84d
Qiaochu Song <qsong6@tmdev.cs.stevens.edu>
2022-03-11 08:45:02 -0800

    no ver num, stable text submodule

--- setup.py
+++ setup.py
@@ -63,11 +63,11 @@ setuptools.setup(
             "hcrystalball==0.1.10",
             "seqeval",
             "mxnet<2.0.0",
-            "autogluon.text==0.3.1",
+            "autogluon.text",
         ],
         "agtextpredictor": [
             "mxnet<2.0.0",
-            "autogluon.text==0.3.1", 
+            "autogluon.text", 
         ], 
         "catboost": ["catboost>=0.26"],
         "blendsearch": ["optuna==2.8.0"],
