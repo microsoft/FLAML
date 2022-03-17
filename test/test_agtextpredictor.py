29cf9c9b548b269bd8dd6d88b3991b590a8dd20c
Varia <Varia@variadembp.mynetworksettings.com>
2022-03-16 22:07:09 -0400

    dependency and import

---
+++ test/test_agtextpredictor.py
@@ -0,0 +1,14 @@
+from flaml import AutoML
+import pandas as pd
+import requests
+import gc
+import numpy as np
+import os
+import sys
+import platform
+from sklearn.model_selection import train_test_split
+
+
+def test_ag_text_predictor():
+    # DEBUG
+    return
