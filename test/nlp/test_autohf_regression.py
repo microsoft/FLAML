8f016decfd0f0624cd837d6510bad58c8f953742
liususan091219 <xliu127@stevens.edu>
2022-03-19 14:09:39 -0400

    addressing comments

--- test/nlp/test_autohf_regression.py
+++ test/nlp/test_autohf_regression.py
@@ -78,6 +78,11 @@ def test_regression():
     }
 
     ray.shutdown()
+    try:
+        ray.init()
+    except RuntimeError:
+        pass
+
     automl.fit(
         X_train=X_train, y_train=y_train, X_val=X_val, y_val=y_val, **automl_settings
     )
