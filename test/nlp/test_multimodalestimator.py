74f27b589adc4dd4f0f9a82e28810987b732791a
Varia <Varia@VariadeMacBook-Pro.local>
2022-04-19 12:31:55 -0400

    reduce text column and budget

--- test/nlp/test_multimodalestimator.py
+++ test/nlp/test_multimodalestimator.py
@@ -27,13 +27,13 @@ def test_multimodalestimator():
             "Around 0335 GMT , Tab shares were up 19 cents , or 4.4 % , at A $ 4.56 , having earlier set a record high of A $ 4.57 .",
             "The stock rose $ 2.11 , or about 11 percent , to close Friday at $ 21.51 on the New York Stock Exchange .",
         ],
-        "sentence2": [
-            'Referring to him as only " the witness " , Amrozi accused his brother of deliberately distorting his evidence .',
-            "Yucaipa bought Dominick 's in 1995 for $ 693 million and sold it to Safeway for $ 1.8 billion in 1998 .",
-            "On June 10 , the ship 's owners had published an advertisement on the Internet , offering the explosives for sale .",
-            "Tab shares jumped 20 cents , or 4.6 % , to set a record closing high at A $ 4.57 .",
-            "PG & E Corp. shares jumped $ 1.63 or 8 percent to $ 21.03 on the New York Stock Exchange on Friday .",
-        ],
+        # "sentence2": [
+        #     'Referring to him as only " the witness " , Amrozi accused his brother of deliberately distorting his evidence .',
+        #     "Yucaipa bought Dominick 's in 1995 for $ 693 million and sold it to Safeway for $ 1.8 billion in 1998 .",
+        #     "On June 10 , the ship 's owners had published an advertisement on the Internet , offering the explosives for sale .",
+        #     "Tab shares jumped 20 cents , or 4.6 % , to set a record closing high at A $ 4.57 .",
+        #     "PG & E Corp. shares jumped $ 1.63 or 8 percent to $ 21.03 on the New York Stock Exchange on Friday .",
+        # ],
         "numerical1": [1, 2, 3, 4, 5],
         "categorical1": ["a", "b", "a", "b", "a"],
         "label": [5, 4, 3, 2, 1],
@@ -43,13 +43,13 @@ def test_multimodalestimator():
                                     test_size=0.2,
                                     random_state=np.random.RandomState(seed))
     
-    feature_columns = ["sentence1", "sentence2", "numerical1", "categorical1"]
-
+    # feature_columns = ["sentence1", "sentence2", "numerical1", "categorical1"]
+    feature_columns = ["sentence1", "numerical1", "categorical1"]
     automl = AutoML()
     automl_settings = {
         "gpu_per_trial": 0,
         "max_iter": 2,
-        "time_budget": 50,
+        "time_budget": 20,
         "task": "mm-regression",
         "metric": "r2",
         "seed": seed,
