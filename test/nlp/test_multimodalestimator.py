c8848c76194b5b10616aaab6d5f019c7730981c5
Varia <Varia@VariadeMacBook-Pro.local>
2022-04-19 13:21:32 -0400

    use only 4-row toy test data

--- test/nlp/test_multimodalestimator.py
+++ test/nlp/test_multimodalestimator.py
@@ -25,25 +25,16 @@ def test_multimodalestimator():
             "Yucaipa owned Dominick 's before selling the chain to Safeway in 1998 for $ 2.5 billion .",
             "They had published an advertisement on the Internet on June 10 , offering the cargo for sale , he added .",
             "Around 0335 GMT , Tab shares were up 19 cents , or 4.4 % , at A $ 4.56 , having earlier set a record high of A $ 4.57 .",
-            "The stock rose $ 2.11 , or about 11 percent , to close Friday at $ 21.51 on the New York Stock Exchange .",
         ],
-        # "sentence2": [
-        #     'Referring to him as only " the witness " , Amrozi accused his brother of deliberately distorting his evidence .',
-        #     "Yucaipa bought Dominick 's in 1995 for $ 693 million and sold it to Safeway for $ 1.8 billion in 1998 .",
-        #     "On June 10 , the ship 's owners had published an advertisement on the Internet , offering the explosives for sale .",
-        #     "Tab shares jumped 20 cents , or 4.6 % , to set a record closing high at A $ 4.57 .",
-        #     "PG & E Corp. shares jumped $ 1.63 or 8 percent to $ 21.03 on the New York Stock Exchange on Friday .",
-        # ],
-        "numerical1": [1, 2, 3, 4, 5],
-        "categorical1": ["a", "b", "a", "b", "a"],
-        "label": [5, 4, 3, 2, 1],
+        "numerical1": [1, 2, 3, 4],
+        "categorical1": ["a", "b", "a", "b"],
+        "label": [5, 4, 3, 2],
     }
     train_dataset = pd.DataFrame(train_data)
     train_dataset, valid_dataset = train_test_split(train_dataset,
                                     test_size=0.2,
                                     random_state=np.random.RandomState(seed))
     
-    # feature_columns = ["sentence1", "sentence2", "numerical1", "categorical1"]
     feature_columns = ["sentence1", "numerical1", "categorical1"]
     automl = AutoML()
     automl_settings = {
