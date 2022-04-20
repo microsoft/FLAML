3a29c5b276ddef0366f79dce923a2892372f6496
Varia <Varia@VariadeMacBook-Pro.local>
2022-04-20 14:23:23 -0400

    give enough test budget

--- test/nlp/test_multimodalestimator.py
+++ test/nlp/test_multimodalestimator.py
@@ -42,7 +42,7 @@ def test_multimodalestimator():
     automl_settings = {
         "gpu_per_trial": 0,
         "max_iter": 2,
-        "time_budget": 10,
+        "time_budget": 20,
         "task": "mm-regression",
         "metric": "r2",
         "seed": seed,
