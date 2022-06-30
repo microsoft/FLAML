043bffa386aef29d4d47ae9aba786096e3eab9da
Xueqing Liu <liususan091219@users.noreply.github.com>
2022-06-30 10:08:14 -0400

    Update flaml/automl.py
    Co-authored-by: Chi Wang <wang.chi@microsoft.com>

--- flaml/automl.py
+++ flaml/automl.py
@@ -1123,7 +1123,7 @@ class AutoML(BaseEstimator):
                     assert (
                         "label_list"
                         in self._state.fit_kwargs_by_estimator[each_estimator]
-                    ), "For the token-classification task, you must either (1) pass token labels; (2) pass id labels and the label list. Please refer to the documentation for more details: [a simple token classification example](https://microsoft.github.io/FLAML/docs/Examples/AutoML-NLP#a-simple-token-classification-example)"
+                    ), "For the token-classification task, you must either (1) pass token labels; or (2) pass id labels and the label list. Please refer to the documentation for more details: https://microsoft.github.io/FLAML/docs/Examples/AutoML-NLP#a-simple-token-classification-example"
 
         self._sample_weight_full = self._state.fit_kwargs.get(
             "sample_weight"
