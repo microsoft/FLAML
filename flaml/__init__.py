5ed69bbfee180bc74db79f40fd3cf929dab0a083
Artin Mohammadi <sheikhartin@gmail.com>
2022-08-07 07:12:23 +0430

    Avoid breaking in case of error
    Co-authored-by: Chi Wang <wang.chi@microsoft.com>

--- flaml/__init__.py
+++ flaml/__init__.py
@@ -2,7 +2,11 @@ from flaml.searcher import CFO, BlendSearch, FLOW2, BlendSearchTuner
 from flaml.automl import AutoML, logger_formatter
 from flaml.onlineml.autovw import AutoVW
 from flaml.version import __version__
-from flaml.utils import *
+try:
+    from flaml.utils import *
+except ImportError:
+    # In case some import from sklearn breaks, don't make `import flaml` fail
+    pass
 import logging
 
 # Set the root logger.
