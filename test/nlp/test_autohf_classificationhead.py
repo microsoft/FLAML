53c9bff09f5e05c7e8acb8ab8e3e505cd690b830
liususan091219 <xliu127@stevens.edu>
2022-10-11 11:36:21 -0400

    run each test separately

--- test/nlp/test_autohf_classificationhead.py
+++ test/nlp/test_autohf_classificationhead.py
@@ -6,12 +6,36 @@ from utils import (
 )
 
 
-def test_switch_classificationhead():
+def test_switch_classificationhead(each_data, each_model_path):
     from flaml import AutoML
     import requests
 
     automl = AutoML()
 
+    X_train, y_train, X_val, y_val = globals()[each_data]()
+    automl_settings = get_automl_settings()
+    automl_settings["model_path"] = each_model_path
+
+    if each_data == "get_toy_data_regression":
+        automl_settings["task"] = "seq-regression"
+        automl_settings["metric"] = "pearsonr"
+    else:
+        automl_settings["task"] = "seq-classification"
+        automl_settings["metric"] = "accuracy"
+
+    try:
+        automl.fit(
+            X_train=X_train,
+            y_train=y_train,
+            X_val=X_val,
+            y_val=y_val,
+            **automl_settings
+        )
+    except requests.exceptions.HTTPError:
+        return
+
+
+if __name__ == "__main__":
     data_list = [
         "get_toy_data_regression",
         "get_toy_data_binclassification",
@@ -25,28 +49,4 @@ def test_switch_classificationhead():
 
     for each_data in data_list:
         for each_model_path in model_path_list:
-            X_train, y_train, X_val, y_val = globals()[each_data]()
-            automl_settings = get_automl_settings()
-            automl_settings["model_path"] = each_model_path
-
-            if each_data == "get_toy_data_regression":
-                automl_settings["task"] = "seq-regression"
-                automl_settings["metric"] = "pearsonr"
-            else:
-                automl_settings["task"] = "seq-classification"
-                automl_settings["metric"] = "accuracy"
-
-            try:
-                automl.fit(
-                    X_train=X_train,
-                    y_train=y_train,
-                    X_val=X_val,
-                    y_val=y_val,
-                    **automl_settings
-                )
-            except requests.exceptions.HTTPError:
-                continue
-
-
-if __name__ == "__main__":
-    test_switch_classificationhead()
+            test_switch_classificationhead(each_data, each_model_path)
