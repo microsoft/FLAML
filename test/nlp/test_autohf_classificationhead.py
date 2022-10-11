8a458e5e29bba3169e051288c8a47947e7ff69cd
liususan091219 <xliu127@stevens.edu>
2022-10-10 23:01:24 -0400

    adding test cases for switch classification head

--- test/nlp/test_autohf_classificationhead.py
+++ test/nlp/test_autohf_classificationhead.py
@@ -1,26 +1,45 @@
-from utils import get_toy_data_multiclassclassification, get_automl_settings
+from utils import (
+    get_toy_data_regression,
+    get_toy_data_binclassification,
+    get_toy_data_multiclassclassification,
+    get_automl_settings,
+)
 
 
-def test_classification_head():
+def test_switch_classificationhead():
     from flaml import AutoML
     import requests
 
-    X_train, y_train, X_val, y_val = get_toy_data_multiclassclassification()
     automl = AutoML()
 
-    automl_settings = get_automl_settings()
+    data_list = [
+        "get_toy_data_regression",
+        "get_toy_data_binclassification",
+        "get_toy_data_multiclassclassification",
+    ]
+    model_path_list = [
+        "textattack/bert-base-uncased-SST-2",
+        "textattack/bert-base-uncased-STS-B",
+        "textattack/bert-base-uncased-MNLI",
+    ]
 
-    try:
-        automl.fit(
-            X_train=X_train,
-            y_train=y_train,
-            X_val=X_val,
-            y_val=y_val,
-            **automl_settings
-        )
-    except requests.exceptions.HTTPError:
-        return
+    for each_data in data_list:
+        for each_model_path in model_path_list:
+            X_train, y_train, X_val, y_val = globals()[each_data]()
+            automl_settings = get_automl_settings()
+            automl_settings["model_path"] = each_model_path
+
+            try:
+                automl.fit(
+                    X_train=X_train,
+                    y_train=y_train,
+                    X_val=X_val,
+                    y_val=y_val,
+                    **automl_settings
+                )
+            except requests.exceptions.HTTPError:
+                return
 
 
 if __name__ == "__main__":
-    test_classification_head()
+    test_switch_classificationhead()
