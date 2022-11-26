054a61ea6303cec0a2f394e7cc85046e87a98f57
Chi Wang <wang.chi@microsoft.com>
2022-11-26 14:56:09 -0800

    skip test in windows

--- test/nlp/test_autohf_tokenclassification.py
+++ test/nlp/test_autohf_tokenclassification.py
@@ -11,8 +11,8 @@ from utils import (
 
 
 @pytest.mark.skipif(
-    sys.platform == "darwin" or sys.version < "3.7",
-    reason="do not run on mac os or py<3.7",
+    sys.platform in ["darwin", "win32"] or sys.version < "3.7",
+    reason="do not run on mac os, windows or py<3.7",
 )
 def test_tokenclassification_idlabel():
     from flaml import AutoML
