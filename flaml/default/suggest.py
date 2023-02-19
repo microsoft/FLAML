ae81d1861723774a0d517e92491e8afe662aa7e5
Mark Harley <mharley.code@gmail.com>
2023-02-19 09:39:35 +0000

    Fix get_classification_objective import in suggest.py

--- flaml/default/suggest.py
+++ flaml/default/suggest.py
@@ -4,9 +4,8 @@ import logging
 import pathlib
 import json
 from flaml.automl.data import DataTransformer
-from flaml.automl.task.task import CLASSIFICATION, Task
+from flaml.automl.task.task import CLASSIFICATION, Task, get_classification_objective
 from flaml.automl.task.factory import task_factory
-from flaml.automl.ml import get_classification_objective
 from flaml.version import __version__
 
 LOCATION = pathlib.Path(__file__).parent.resolve()
