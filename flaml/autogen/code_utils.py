51066f2ef74722deac5a1d8a5461ff7267bcfc09
Chi Wang <wang.chi@microsoft.com>
2023-04-22 18:09:16 +0000

    path

--- flaml/autogen/code_utils.py
+++ flaml/autogen/code_utils.py
@@ -191,7 +191,7 @@ def execute_code(
                 print("Failed to pull image", image)
     # get a randomized str based on current time to wrap the exit code
     exit_code_str = f"exitcode{time.time()}"
-    abs_path = pathlib.Path(work_dir).absolute().as_posix()
+    abs_path = pathlib.Path(work_dir).absolute()  # .as_posix()
     # if sys.platform == "win32":
     #     abs_path = f"/{abs_path[0].lower()}{abs_path[2:]}"
     # create a docker container
