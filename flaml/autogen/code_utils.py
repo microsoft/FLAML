e0f5febc5ecdc31f5fc895cc2eae59d8073c2bc5
Chi Wang <wang.chi@microsoft.com>
2023-04-22 19:55:00 +0000

    path

--- flaml/autogen/code_utils.py
+++ flaml/autogen/code_utils.py
@@ -193,7 +193,7 @@ def execute_code(
     exit_code_str = f"exitcode{time.time()}"
     abs_path = pathlib.Path(work_dir).absolute()
     if sys.platform == "win32":
-        abs_path = abs_path.replace("\\", "/")
+        abs_path = str(abs_path).replace("\\", "/")
         abs_path = f"//{abs_path[0].lower()}{abs_path[2:]}"
     # create a docker container
     container = client.containers.run(
