8c5a86c1827b2918cb2077f3ba3c0b698a45519f
kevin666aa <yrwu000627@gmail.com>
2023-04-24 17:34:54 -0400

    change prompt back to use fractions instead of decimal

--- flaml/autogen/math/math_solver.py
+++ flaml/autogen/math/math_solver.py
@@ -9,7 +9,7 @@ from openai.error import InvalidRequestError, RateLimitError, Timeout
 from utils import write_json, remove_asy_sections, math_type_mapping
 
 PROMPTS = {
-    "select": """Let's use two tools (python code and Wolfram alpha) to solve a math problem step by step. You should always follow your own reasoning and only query when necessary. You should always use fractions or radical form instead of decimal when writing python code (use sympy).
+    "select": """Let's use two tools (python code and Wolfram alpha) to solve a math problem step by step. You should always follow your own reasoning and only query when necessary.
 
 First state the key idea to solve the problem. Then follow the process:
 1. Continue the solving steps until you need to query.
@@ -18,7 +18,7 @@ Please format the query in json:
 { "tool" : "", # "python" or "wolfram"
 "query": "", # your query here, either python code or Wolfram query.
 }
-Note: when you put python code in the query, you should: 1.make sure the indentation is correct(use '\\t'). 2. use 'print' function for the output.
+Note: when you put python code in the query, you must: 1.always use fractions instead of decimal 2.make sure the indentation is correct(use '\\t'). 3. use 'print' function for the output.
 4. Wait for me to give the results.
 5. Correct this step based on the results, or give a new query if the results are invalid.
 6. When you get the answer, put the answer in \\boxed{}.
@@ -28,7 +28,7 @@ Note: when you put python code in the query, you should: 1.make sure the indenta
 
 First state the key idea to solve the problem. Then follow the process:
 1. Continue the solving steps until you need to query.
-2. Take out any queries that can be asked through python (for example, any calculations or equations that can be calculated). When you are querying python, you should: 1. always use fractions or radical form instead of decimal (use sympy) 2.make sure the indentation is correct(use '\\t'). 3. use 'print' function for the output.
+2. Take out any queries that can be asked through python (for example, any calculations or equations that can be calculated). When you are querying python, you should: 1. always use fractions instead of decimal 2.make sure the indentation is correct(use '\\t'). 3. use 'print' function for the output.
 Please format the query in json:
 { "tool" : "python",
 "query": "", # your code here.
