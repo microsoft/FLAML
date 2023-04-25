2b8b717a6241553e5a343e481a2f5fbb92eafaf9
kevin666aa <yrwu000627@gmail.com>
2023-04-24 20:11:21 -0400

    rewind prompt back to e37ee3

--- flaml/autogen/math/query_handler.py
+++ flaml/autogen/math/query_handler.py
@@ -28,10 +28,10 @@ class QueryHandler:
         """
         queries = self.extractJSON(response)  # extract json queries
         if len(queries) == 0:
-            if "tool" in response and "query" in response:
+            if 'tool' in response and 'query' in response:
                 return "No query found. Please make sure your query is in correct JSON format.", False
             else:
-                return "Continue. Stop only when you need to query.", True
+                return "Continue", True
         self.total_q_count += len(queries)
         self.valid_q_count += len(queries)
 
