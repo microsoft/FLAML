955edca8033a8a14b5759147bc44d13b3bb84915
kevin666aa <yrwu000627@gmail.com>
2023-04-27 04:49:26 -0400

    change run sequence

--- flaml/autogen/math/run.sh
+++ flaml/autogen/math/run.sh
@@ -10,8 +10,9 @@
 python main.py --prompt_type v2select --prompt_location user --categories 0
 python main.py --prompt_type v1nostep --prompt_location user --categories 0
 
+python main.py --prompt_type v1refine --prompt_location user --categories 0
+python main.py --prompt_type v2refine --prompt_location user --categories 0
+
 python main.py --prompt_type v2select --prompt_location user --categories 1
 python main.py --prompt_type v1nostep --prompt_location user --categories 1
 
-python main.py --prompt_type v1refine --prompt_location user --categories 0
-python main.py --prompt_type v2refine --prompt_location user --categories 0
