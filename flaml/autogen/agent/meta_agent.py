5c2eac26a7c71ab15ff7cf42c213832d4995686f
Qingyun Wu <qingyun.wu@psu.edu>
2023-05-12 22:15:57 -0400

    update meta agent

--- flaml/autogen/agent/meta_agent.py
+++ flaml/autogen/agent/meta_agent.py
@@ -2,12 +2,11 @@ from collections import defaultdict
 from flaml.autogen.agent.agent import Agent
 from flaml.autogen.agent.chat_agent import ChatAgent
 from flaml.autogen.agent.coding_agent import PythonAgent
-from langchain import OpenAI, LLMChain, PromptTemplate
 from flaml import oai
 from flaml.autogen.code_utils import DEFAULT_MODEL, FAST_MODEL
 
 
-class MetaAgent:
+class MetaAgent(Agent):
     """(Experimental) A meta agent that can wrap other agents and perform actions based on the messages received."""
 
     DEFAULT_CONFIG = {
@@ -51,20 +50,21 @@ class MetaAgent:
         self._conversations = defaultdict(list)
         self._name = meta_agent_name
         self._config = self.DEFAULT_CONFIG
-        self._system_prompt_glossary = {
-            ChatAgent.__name__: ChatAgent.DEFAULT_SYSTEM_MESSAGE,
-            PythonAgent.__name__: PythonAgent.DEFAULT_SYSTEM_MESSAGE,
-        }
-        self._system_message = (
-            meta_agent_system_message
-            if meta_agent_system_message
-            else self._system_prompt_glossary[agent_class.__name__]
-        )
+        self._system_message = meta_agent_system_message if meta_agent_system_message else self.DEFAULT_SYSTEM_MESSAGE
         self._agent_class = agent_class
+        if system_message:
+            self._agent = agent_class(name, system_message=system_message)
+        else:
+            self._agent = agent_class(name)
 
-        self._agent = agent_class(name)
-        print("Agent class", self._agent_class)
-        self._meta_template = """
+        # TODO: Maintain a dictionary of meta prompts for each agent class
+        # self._meta_prompt_glossary = {
+        #     ChatAgent.__name__: ChatAgent.DEFAULT_SYSTEM_MESSAGE,
+        #     PythonAgent.__name__: PythonAgent.DEFAULT_SYSTEM_MESSAGE,
+        # }
+
+        # the following meta prompt is based on https://noahgoodman.substack.com/p/meta-prompt-a-simple-self-improving
+        self._meta_prompt_template = """
         Assistant has just had the below interactions with a User. Assistant followed their "Instructions" closely.
         Your job is to critique the Assistant's performance and then revise the Instructions so that Assistant would
         quickly and correctly respond in the future.
@@ -77,20 +77,18 @@ class MetaAgent:
 
         Please reflect on these interactions.
 
-        You should first critique Assistant's performance. What could Assistant have done better?
+        You should first reflect on Assistant's performance. What could Assistant have done better?
         What should the Assistant remember about this user? Are there things this user always wants?
-        Indicate this with "Critique: ...".
+        Indicate this with "Reflection: ...".
 
         You should next revise the Instructions so that Assistant would quickly and correctly respond in the future.
-        Assistant's goal is to satisfy the user in as few interactions as possible. Assistant will only see the new Instructions, not the interaction history, so anything important must be summarized in the Instructions. Don't forget any important details in the current Instructions! Indicate the new Instructions by "Instructions: ...".
+        Assistant's goal is to satisfy the user in as few interactions as possible. Assistant will only see the new Instructions, not the interaction history, so anything important must be summarized in the Instructions.
+        Indicate the new Instructions by "Instructions: ...".
         """
 
     def _receive(self, message, sender):
         """Receive a message from another agent."""
         self._agent.receive(message, sender)
-
-    def _init_reflect(self):
-        PromptTemplate(input_variables=["chat_history"], template=self._system_message)
 
     def _get_chat_history(self):
         """Get the chat history of the agent."""
@@ -103,38 +101,12 @@ class MetaAgent:
                     chat_history += "Assistant: " + message["content"] + "\n"
         return chat_history
 
-    def _new_task_bookkeeping(self):
-        """Bookkeeping for a new task."""
-        self._agent._conversations = defaultdict(list)
-        self._agent._memory = []
-        self._agent._sender_dict = {}
-
     def reflect(self):
         """Reflect on the conversations with the agents."""
         chat_history = self._get_chat_history()
-        # PromptTemplate(
-        #     input_variables=["chat_history"],
-        #     template=self._meta_template
-        # )
-        meta_prompt = self._meta_template.format(chat_history=chat_history)
-
+        meta_prompt = self._meta_prompt_template.format(chat_history=chat_history)
         responses = oai.ChatCompletion.create(messages=[{"content": meta_prompt, "role": "user"}], **self._config)
         response = oai.ChatCompletion.extract_text(responses)[0]
-        print(f"{self._name}", response)
-
-        # new_instructions = self._get_new_instructions()
-        # self._init_reflect()
-        # self._agent._send(chat_history, self)
-        # self._agent._send(new_instructions, self)
-        # self._system_message = new_instructions
+        print(f"Reflecting.....\n{self._name}", response)
         self._agent = self._agent_class(self._name, meta_prompt=response)
         # TODO: maybe we should also consider adding the instruction as the init prompt
-
-    def receive(self, message, sender, is_new_task=False):
-        """Receive a message from another agent.
-        This method is called by the sender.
-        It needs to be overriden by the subclass to perform followup actions.
-        """
-        print(f'{self._name} received "{message}" from {sender._name}')
-        self._receive(message, sender)
-        # perform actions based on the message
--- test/autogen/test_meta_agent.py
+++ test/autogen/test_meta_agent.py
@@ -8,26 +8,21 @@ from flaml.autogen.agent.meta_agent import MetaAgent
 
 
 def test_meta_prompt():
-    """
-    Based on:
-    https://python.langchain.com/en/latest/use_cases/autonomous_agents/meta_prompt.html
-    """
-
     tasks = [
         "Provide a systematic argument for why we should always eat pasta with olives.",
         "Provide a systematic argument for why we should always eat noodle with garlic.",
         "What should I eat for dinner",
         "What should I eat for lunch",
     ]
+    # Reply: Your response should be in the form of a poem. Try again!
     oai.ChatCompletion.start_logging()
     agent = MetaAgent(ChatAgent, name="chat_agent", meta_agent_name="meta_agent")
-    user = HumanProxyAgent(
-        "human user", work_dir="test/autogen", human_input_mode="ALWAYS", max_consecutive_auto_reply=10
-    )
+    user = HumanProxyAgent("human user", work_dir="test/autogen", human_input_mode="ALWAYS")
     for i, task in enumerate(tasks):
+        print(f".........Starting the {i+1}-th task!.........")
         agent.receive(task, user)
-        print(f"====={i+1}-th task finished!=====")
         agent.reflect()
+        print(f".........{i+1}-th task finished!.........")
 
 
 if __name__ == "__main__":
