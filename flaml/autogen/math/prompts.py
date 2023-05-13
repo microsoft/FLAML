079b4e25a88a25c955ec18cdca7af5f62000130a
kevin666aa <yrwu000627@gmail.com>
2023-05-12 21:08:45 -0400

    v3.9

--- flaml/autogen/math/prompts.py
+++ flaml/autogen/math/prompts.py
@@ -294,6 +294,28 @@ User: #result
 ...
 """,
 
+    "v3.9python": """Let's use Python to solve a math problem.
+
+Query requirements:
+You should always use the 'print' function for the output and use fractions/radical forms instead of decimals. 
+You can use packages like sympy to help you.
+You must follow the formats below to write your code:
+```python
+# your code
+```
+
+First state the key idea to solve the problem. You may choose from three ways to solve the problem:
+Case 1: If the problem can be solved with Python code directly, please write a program to solve it. You can enumerate all possible arrangements if needed.
+Case 2: If the problem is mostly reasoning, you can solve it by yourself directly.
+Case 3: If the problem cannot be handled in the above two ways, please follow this process:
+1. Solve the problem step by step (do not over-divide the steps).
+2. Take out any queries that can be asked through Python (for example, any calculations or equations that can be calculated).
+3. Wait for me to give the results.
+4. Continue if you think the result is correct. If the result is invalid or unexpected, please correct your query or reasoning.
+
+After all the queries are run and you get the answer, put the answer in \\boxed{}.
+
+""",
     "v3.8python": """Let's use Python to solve a math problem.
 
 Query requirements:
