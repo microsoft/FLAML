e4b9790723428ba93ce7122de9b3920cfb923bed
Chi Wang <wang.chi@microsoft.com>
2023-05-21 15:02:57 +0000

    test

--- test/autogen/oai/test_notebook.py
+++ test/autogen/oai/test_notebook.py
@@ -1,7 +1,4 @@
 import sys
-import nbformat
-from nbconvert.preprocessors import ExecutePreprocessor
-from nbconvert.preprocessors import CellExecutionError
 import os
 import pytest
 
@@ -17,29 +14,33 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 
 def run_notebook(input_nb, output_nb="executed_openai_notebook.ipynb", save=False):
+    import nbformat
+    from nbconvert.preprocessors import ExecutePreprocessor
+    from nbconvert.preprocessors import CellExecutionError
+
     try:
         file_path = os.path.join(here, os.pardir, os.pardir, "notebook", input_nb)
-        with open(file_path) as f:
-            nb = nbformat.read(f, as_version=4)
-        ep = ExecutePreprocessor(timeout=4800, kernel_name="python3")
-        ep.preprocess(nb, {"metadata": {"path": here}})
+        with open(file_path) as nb_file:
+            nb = nbformat.read(nb_file, as_version=4)
+        preprocessor = ExecutePreprocessor(timeout=4800, kernel_name="python3")
+        preprocessor.preprocess(nb, {"metadata": {"path": here}})
 
         output_file_name = "executed_openai_notebook_output.txt"
         output_file = os.path.join(here, output_file_name)
-        with open(output_file, "a") as f:
+        with open(output_file, "a") as nb_output_file:
             for cell in nb.cells:
                 if cell.cell_type == "code" and "outputs" in cell:
                     for output in cell.outputs:
                         if "text" in output:
-                            f.write(output["text"].strip() + "\n")
+                            nb_output_file.write(output["text"].strip() + "\n")
                         elif "data" in output and "text/plain" in output["data"]:
-                            f.write(output["data"]["text/plain"].strip() + "\n")
+                            nb_output_file.write(output["data"]["text/plain"].strip() + "\n")
     except CellExecutionError:
         raise
     finally:
         if save:
-            with open(os.path.join(here, output_nb), "w", encoding="utf-8") as f:
-                nbformat.write(nb, f)
+            with open(os.path.join(here, output_nb), "w", encoding="utf-8") as nb_executed_file:
+                nbformat.write(nb, nb_executed_file)
 
 
 @pytest.mark.skipif(
