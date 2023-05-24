9bb46551dbe8b55d22c9422f3e8508974625c6c6
Qingyun Wu <qingyun.wu@psu.edu>
2023-05-24 15:28:57 -0400

    update meta agent test

--- test/autogen/test_meta_agent.py
+++ test/autogen/test_meta_agent.py
@@ -1,33 +1,42 @@
-from flaml.autogen.agent.chat_agent import ChatAgent
 from flaml.autogen.agent.coding_agent import PythonAgent
-
-from flaml.autogen.agent.human_proxy_agent import HumanProxyAgent
-from flaml.autogen.math_utils import eval_math_responses, get_answer
-from flaml import oai
+from flaml.autogen.agent.user_proxy_agent import UserProxyAgent
 from flaml.autogen.agent.meta_agent import MetaAgent
 
 
 def test_meta_prompt():
     tasks = [
-        "Provide a systematic argument for why we should always eat pasta with olives.",
-        "Provide a systematic argument for why we should always eat noodle with garlic.",
-        "What should I eat for dinner",
-        "What should I eat for lunch",
+        "Create and execute a script to plot a rocket without using matplotlib.",
+        "Create and execute a script to plot a helicopter without using matplotlib",
     ]
-    # Reply: Your response should be in the form of a poem. Try again!
-    oai.ChatCompletion.start_logging()
-    agent = MetaAgent(ChatAgent, name="chat_agent", meta_agent_name="meta_agent")
-    user = HumanProxyAgent("human user", work_dir="test/autogen", human_input_mode="ALWAYS")
+
+    ## User mode:
+    python_agent = PythonAgent("python agent")
+    assistant = MetaAgent(name="meta agent", agent=python_agent)
+    user = UserProxyAgent("human user", work_dir="test/autogen", human_input_mode="ALWAYS")
     for i, task in enumerate(tasks):
         print(f".........Starting the {i+1}-th task!.........")
-        agent.receive(task, user)
-        agent.reflect()
+        assistant.receive(task, user)
+        print(f".........{i+1}-th task finished!.........")
+
+    ## Dev mode:
+    dev = UserProxyAgent("expert", work_dir="test/autogen", human_input_mode="ALWAYS")
+    assistant = MetaAgent(name="meta agent", agent=python_agent, dev_agent=dev)
+    user = UserProxyAgent("human user", work_dir="test/autogen", human_input_mode="ALWAYS")
+    for i, task in enumerate(tasks[0:2]):
+        assistant.receive(task, user)
+        assistant.reflect()
+
+    ### Can also be used in the following way:
+    for i, task in enumerate(tasks):
+        print(f".........Starting the {i+1}-th task!.........")
+        assistant.receive(task, dev)
+        assistant.reflect()
         print(f".........{i+1}-th task finished!.........")
 
 
 if __name__ == "__main__":
     import openai
 
-    # openai.api_key_path = "test/openai/key.txt"
-    openai.api_key_path = "test/openai/key_gpt3.txt"
+    openai.api_key_path = "test/openai/key.txt"
+    # openai.api_key_path = "test/openai/key_gpt3.txt"
     test_meta_prompt()
