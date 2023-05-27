52b13ec1f024ee7388c828d72478001ccd24f431
Chi Wang <wang.chi@microsoft.com>
2023-05-27 00:26:59 +0000

    functioning

--- flaml/autogen/datagen.py
+++ flaml/autogen/datagen.py
@@ -1,10 +1,8 @@
 import json
-import regex as re
-import numpy as np
 from flaml import oai
 
 
-def generate_adversarial_examples(data, verif_func, eval_func, num_examples=5, reduction=np.mean, **config):
+def generate_adversarial_examples(data, verif_func, eval_func, num_examples=5, **config):
     base_prompt = "Generate more complex versions of the following input examples. Make sure that the input would result in the same target as specified. Make sure that the inputs are of the same types that are specified in the examples. Generate parsable json with double quotes. Do not replace integers with words.\nexamples:{examples}"
 
     # base_settings = {
@@ -15,9 +13,9 @@ def generate_adversarial_examples(data, verif_func, eval_func, num_examples=5, r
     #     "model": "gpt-4",
     # }
     max_iter = 10
-    iter = 0
+    iteration = 0
     adv_examples = []
-    while len(adv_examples) < num_examples and iter < max_iter:
+    while len(adv_examples) < num_examples and iteration < max_iter:
         # query = base_settings
         # query["prompt"] = base_prompt.format(examples=str(data))
         response = oai.Completion.create({"examples": str(data)}, prompt=base_prompt, **config)
@@ -29,5 +27,5 @@ def generate_adversarial_examples(data, verif_func, eval_func, num_examples=5, r
             cand_test = eval_func(candidate)
             if cand_verification and not cand_test:
                 adv_examples.append(candidate)
-
+        iteration += 1
     return adv_examples
