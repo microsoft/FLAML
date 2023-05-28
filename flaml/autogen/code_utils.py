edb60aee753ff6369d4de728e167a640e2868049
Chi Wang <wang.chi@microsoft.com>
2023-05-28 14:27:59 +0000

    docker

--- flaml/autogen/code_utils.py
+++ flaml/autogen/code_utils.py
@@ -240,7 +240,8 @@ def execute_code(
     if lang.startswith("python"):
         cmd = [
             "sh",
-            f"-c {lang} {filename}; exit_code=$?; echo -n {exit_code_str}; echo -n $exit_code; echo {exit_code_str}",
+            "-c",
+            f"{lang} {filename}; exit_code=$?; echo -n {exit_code_str}; echo -n $exit_code; echo {exit_code_str}",
         ]
     else:
         cmd = [
