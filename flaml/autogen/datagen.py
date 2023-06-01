1ec0f7e105462af32001259cacacf74dbda5c666
Srinagesh Sharma <srsharm@microsoft.com>
2023-06-01 19:08:22 -0400

    Adding Hydra to AdvGen, WikiQA test case

--- flaml/autogen/datagen.py
+++ flaml/autogen/datagen.py
@@ -1,9 +1,25 @@
 import json
 from flaml import oai
+import regex as re
+from itertools import compress
+import time
+import logging
 
+logger = logging.getLogger(__name__)
 
 def generate_adversarial_examples(data, verif_func, eval_func, num_examples=5, **config):
-    base_prompt = "Generate more complex versions of the following input examples. Make sure that the input would result in the same target as specified. Make sure that the inputs are of the same types that are specified in the examples. Generate parsable json with double quotes. Do not replace integers with words.\nexamples:{examples}"
+    base_prompt = """ 
+    # Instructions
+    - Generate adversarial versions of the examples in the following task.
+    - Make sure that the input would result in the same target as specified. 
+    - Make sure that the inputs are of the same types that are specified in the examples. 
+    - Generate parsable json with double quotes. 
+    - Do not replace integers with words.
+    <|start|>(example)
+    {example}
+    <|end|>
+    <|start|>(answer)
+    """
 
     # base_settings = {
     #     "max_tokens": 64,
@@ -15,17 +31,42 @@ def generate_adversarial_examples(data, verif_func, eval_func, num_examples=5, *
     max_iter = 10
     iteration = 0
     adv_examples = []
+
+    def group_check(candidate): # replace with loss function
+        verif = verif_func(candidate)
+        cand_test = eval_func(candidate)
+        return verif and not cand_test
+
     while len(adv_examples) < num_examples and iteration < max_iter:
         # query = base_settings
         # query["prompt"] = base_prompt.format(examples=str(data))
-        response = oai.Completion.create({"examples": str(data)}, prompt=base_prompt, **config)
-        resp = oai.Completion.extract_text(response)[0]
-        adv_candidates = json.loads(resp.strip().replace("'", '"'))  # re.findall(r"(?={).*(?<=})", resp)
-        for cand in adv_candidates:
-            candidate = cand
-            cand_verification = verif_func(candidate)
-            cand_test = eval_func(candidate)
-            if cand_verification and not cand_test:
-                adv_examples.append(candidate)
-        iteration += 1
+        # time.sleep(62)
+        response = oai.Completion.create({"example": str(data)}, prompt=base_prompt, **config)
+        resp_candidates = re.findall(r"(?={).*(?<=})", oai.Completion.extract_text(response)[0])
+        adv_candidates = list(map(eval, resp_candidates))
+        eval_candidates = list(map(group_check, adv_candidates))
+        valid_candidates = list(compress(adv_candidates, eval_candidates))
+        if len(valid_candidates) > 0:
+            adv_examples.append(valid_candidates)
+            iteration = 0
+        else:
+            iteration += 1
+
     return adv_examples
+
+
+# base_prompt = """
+    # <|meta_start|>
+    # # Introduction
+    # - You are an adversarial example generation assistant
+    # - Your goal is to generate more complex versions of the examples in the following task. 
+    # - Make sure that the input would result in the same target as specified. 
+    # - Make sure that the inputs are of the same types that are specified in the examples. 
+    # - Generate parsable json with double quotes. 
+    # - Do not replace integers with words.
+    # <|meta_end|>
+    # <|start|>(example)
+    # {examples}
+    # <|end|>
+    # <|start|>(answer)
+    # """
--- test/autogen/test_adv_gen.py
+++ test/autogen/test_adv_gen.py
@@ -1,56 +1,64 @@
 from flaml import oai
 from flaml.autogen.datagen import generate_adversarial_examples
 import re
+import logging
+import hydra
+import wikipedia
 
 KEY_LOC = "test/autogen"
+logger = logging.getLogger(__name__)
 
-
-def test_adv_gen():
+@hydra.main(config_path="configs", config_name="config")
+def test_adv_gen(cfg):
     try:
         import openai
     except ImportError:
         return
 
+    # config_list_adv = oai.config_list_gpt4_gpt35(KEY_LOC)
+    config_list_adv = oai.config_list_openai_aoai(KEY_LOC)
+    config_list_adv[0].update(cfg.openai.adv)
+    config_list_eval = oai.config_list_openai_aoai(KEY_LOC)
+    config_list_eval[0].update(cfg.openai.eval)
+
+    test_cases = [# SimpleArith(config_list=config_list_eval)
+        WikipediaQGen(config_list=config_list_eval)
+        ]
+
+    for case in test_cases:
+        adv_examples = generate_adversarial_examples(
+            data=case.input_examples,
+            verif_func=case.verif_func,
+            eval_func=case.test_func,
+            num_examples=5,
+            # reduction=np.mean,
+            config_list=config_list_adv,
+        )
+        print(adv_examples)
+
+class SimpleArith:
     input_examples = [
         {"input": "1 + 4 =", "target": "5"},
         {"input": "4 + 9 =", "target": "13"},
-        {"input": "5 + 0 =", "target": "5"},
-        {"input": "6 + 4 =", "target": "10"},
         {"input": "8 + 3 =", "target": "11"},
-        {"input": "5 + 6 =", "target": "11"},
-        {"input": "5 + 2 =", "target": "7"},
-        {"input": "8 + 1 =", "target": "9"},
-        {"input": "0 + 4 =", "target": "4"},
-        {"input": "3 + 3 =", "target": "6"},
-        {"input": "47 + 8 =", "target": "55"},
         {"input": "30 + 89 =", "target": "119"},
-        {"input": "30 + 58 =", "target": "88"},
-        {"input": "31 + 46 =", "target": "77"},
-        {"input": "16 + 72 =", "target": "88"},
-        {"input": "15 + 63 =", "target": "78"},
-        {"input": "34 + 31 =", "target": "65"},
-        {"input": "34 + 17 =", "target": "51"},
-        {"input": "9 + 74 =", "target": "83"},
-        {"input": "86 + 33 =", "target": "119"},
         {"input": "486 + 141 =", "target": "627"},
-        {"input": "76 + 812 =", "target": "888"},
         {"input": "13 + 476 =", "target": "489"},
-        {"input": "94 + 129 =", "target": "223"},
-        {"input": "635 + 613 =", "target": "1248"},
-        {"input": "516 + 962 =", "target": "1478"},
-        {"input": "749 + 750 =", "target": "1499"},
-        {"input": "389 + 442 =", "target": "831"},
         {"input": "773 + 546 =", "target": "1319"},
         {"input": "348 + 227 =", "target": "575"},
     ]
 
-    def verif_arith(example):
+    def __init__(self, config_list):
+        self.config_list = config_list
+
+    @staticmethod
+    def verif_func(example):
         lhs = eval(re.findall(r"^(.*?)=", example["input"])[0].strip())
         rhs = int(example["target"])
 
         return lhs == rhs
 
-    def test_arith(example):
+    def test_func(self, example):
         base_prompt = "{input}"
         config = {
             "max_tokens": 64,
@@ -59,30 +67,117 @@ def test_adv_gen():
             "n": 1,
             "stream": False,
             "model": "text-davinci-003",
-            "stop": "\n",
         }
         # query['prompt'] = base_prompt.format(example['input'])
         # resp = oai.Completion.create(**query)
-        response = oai.Completion.create(example, prompt=base_prompt, config_list=config_list_eval, **config)
+        response = oai.Completion.create(example, prompt=base_prompt, config_list=self.config_list, **config)
         return example["target"] == oai.Completion.extract_text(response)[0].strip()
 
-    config_list_adv = oai.config_list_gpt4_gpt35(KEY_LOC)
-    config_list_eval = oai.config_list_openai_aoai(KEY_LOC)
-    adv_examples = generate_adversarial_examples(
-        data=input_examples,
-        verif_func=verif_arith,
-        eval_func=test_arith,
-        num_examples=5,
-        # reduction=np.mean,
-        config_list=config_list_adv,
-    )
-    print(adv_examples)
 
-    # adv_examples, metric_change = oai.Completion.generate_adversarial_examples(data=input_examples, mode, eval_func, num_examples)
+class WikipediaQGen:
+    def __init__(self, config_list, search_term='Cornell University'):
+        self.config_list = config_list
+        r = wikipedia.search(search_term)
+        page = wikipedia.page(r[0])
+        self.title = page.title
+        self.content = page.content
+        example_gen_prompt = f"""<|im_start|>system
+You are a question generating assistant. Your objective is to take some context and generate questions together with their corresponding answer or possible answers
+<|im_end|>
+<|im_start|>user
+Context
+---
+# 
+{page.title}
 
-    # adv_examples are like ...
+{page.content}
+<|im_end|>
+<|im_start|>user
+Generate a series of questions related to {page.title} as follows.
 
-    # metric is changed from ... to ...
+1. Mode = "paragraph"
+
+Write a question for which the answer is a short paragraph.
+
+2. Mode = "few-words"
+
+The answer is at most a few words.
+
+3. Mode = "number"
+
+The answer is a number.
+
+4. Mode = "bool"
+
+Generate a question with a True/False answer.
+
+For each question above, provide the corresponding correct answer. If there is more than one correct answer, provide a list of all possible answers.
+<|im_end|>
+<|im_start|>assistant
+"""
+        config = {
+            "max_tokens": 512,
+            "temperature": 0.7,
+            "top_p": 1,
+            "n": 1,
+            "stream": False,
+            "model": "text-davinci-003",
+        }
+
+        response = oai.Completion.create(prompt=example_gen_prompt, config_list=self.config_list, **config)
+        answer = oai.Completion.extract_text(response)[0].strip()
+        # find qa
+        qa_parsed = re.findall(r"(?=Question:)[\s\S]*?(?=[0-9]. Mode|$)", response)
+        self.input_examples = []
+        for qa in qa_parsed:
+            example = {"input":re.findall(r"(?<=Question:)[\s\S]*?(?=Answer:)", qa)[0].strip(), 
+                       "target":re.findall(r"(?<=Answer:)", qa)[0].strip()}
+            self.input_examples.append(example)
+
+
+    def add_message(self, content, role="user"):
+        self.messages.append({"role": role, "content": content})
+
+    def verif_func(self, example):
+        base_prompt = """Respond with Yes or No, does the text below answer the question provided?
+        Question: {input}
+        Text: {target}
+        Answer:
+        """
+        config = {
+            "max_tokens": 512,
+            "temperature": 0,
+            "top_p": 1,
+            "n": 1,
+            "stream": False,
+            "model": "text-davinci-003",
+        }
+        response = oai.Completion.create(example, prompt=base_prompt, config_list=self.config_list, **config)
+        answer = oai.Completion.extract_text(response)[0].strip()
+        return answer == 'Yes'
+
+    def test_func(self, example):
+        base_prompt = f"""Answer the following question based on the context provided.
+        Question:
+        {{input}}
+        Context:
+        {self.title}
+        {self.content}
+        Answer:
+        """
+        config = {
+            "max_tokens": 512,
+            "temperature": 0,
+            "top_p": 1,
+            "n": 1,
+            "stream": False,
+            "model": "text-davinci-003",
+        }
+        response = oai.Completion.create(example, prompt=base_prompt, config_list=self.config_list, **config)
+        answer = oai.Completion.extract_text(response)[0]
+        pred_example = {"input": example["input"], "target": answer}
+        return self.verif_func(pred_example)
+
 
 
 if __name__ == "__main__":
