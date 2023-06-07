946c53d3befae19c1fc238a220b63724e0712831
Chi Wang <wang.chi@microsoft.com>
2023-06-07 05:34:18 +0000

    docker

--- test/autogen/test_assistant_agent.py
+++ test/autogen/test_assistant_agent.py
@@ -25,6 +25,7 @@ def test_gpt35(human_input_mode="NEVER", max_consecutive_auto_reply=5):
         human_input_mode=human_input_mode,
         is_termination_msg=lambda x: x.rstrip().endswith("TERMINATE"),
         max_consecutive_auto_reply=max_consecutive_auto_reply,
+        use_docker="python:3",
     )
     coding_task = "Print hello world to a file called hello.txt"
     assistant.receive(coding_task, user)
