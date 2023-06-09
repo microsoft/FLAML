e9a0e6848a5501623960d869e65835c490c12732
kevin666aa <yrwu000627@gmail.com>
2023-06-08 23:57:17 -0400

    update notebook and mathagent

--- flaml/autogen/agent/math_user_proxy_agent.py
+++ flaml/autogen/agent/math_user_proxy_agent.py
@@ -8,6 +8,79 @@ from pydantic import BaseModel, Extra, root_validator
 from typing import Any, Dict, Optional
 from time import sleep
 
+
+PROMPTS = {
+    # default
+    "default": """Let's use Python to solve a math problem.
+
+Query requirements:
+You should always use the 'print' function for the output and use fractions/radical forms instead of decimals.
+You can use packages like sympy to help you.
+You must follow the formats below to write your code:
+```python
+# your code
+```
+
+First state the key idea to solve the problem. You may choose from three ways to solve the problem:
+Case 1: If the problem can be solved with Python code directly, please write a program to solve it. You can enumerate all possible arrangements if needed.
+Case 2: If the problem is mostly reasoning, you can solve it by yourself directly.
+Case 3: If the problem cannot be handled in the above two ways, please follow this process:
+1. Solve the problem step by step (do not over-divide the steps).
+2. Take out any queries that can be asked through Python (for example, any calculations or equations that can be calculated).
+3. Wait for me to give the results.
+4. Continue if you think the result is correct. If the result is invalid or unexpected, please correct your query or reasoning.
+
+After all the queries are run and you get the answer, put the answer in \\boxed{}.
+
+""",
+    # select python or wolfram
+    "two_tools": """Let's use two tools (Python and Wolfram alpha) to solve a math problem.
+
+Query requirements:
+You must follow the formats below to write your query:
+For Wolfram Alpha:
+```wolfram
+# one wolfram query
+```
+For Python:
+```python
+# your code
+```
+When using Python, you should always use the 'print' function for the output and use fractions/radical forms instead of decimals. You can use packages like sympy to help you.
+When using wolfram, give one query in each code block.
+
+Please follow this process:
+1. Solve the problem step by step (do not over-divide the steps).
+2. Take out any queries that can be asked through Python or Wolfram Alpha, select the most suitable tool to be used (for example, any calculations or equations that can be calculated).
+3. Wait for me to give the results.
+4. Continue if you think the result is correct. If the result is invalid or unexpected, please correct your query or reasoning.
+
+After all the queries are run and you get the answer, put the final answer in \\boxed{}.
+
+Problem: """,
+    # use python step by step
+    "python": """Let's use Python to solve a math problem.
+
+Query requirements:
+You should always use the 'print' function for the output and use fractions/radical forms instead of decimals.
+You can use packages like sympy to help you.
+You must follow the formats below to write your code:
+```python
+# your code
+```
+
+Please follow this process:
+1. Solve the problem step by step (do not over-divide the steps).
+2. Take out any queries that can be asked through Python (for example, any calculations or equations that can be calculated).
+3. Wait for me to give the results.
+4. Continue if you think the result is correct. If the result is invalid or unexpected, please correct your query or reasoning.
+
+After all the queries are run and you get the answer, put the answer in \\boxed{}.
+
+Problem: """,
+}
+
+
 class MathUserProxyAgent(UserProxyAgent):
     """(Experimental) A MathChat agent that can handle math problems."""
 
@@ -15,10 +88,10 @@ class MathUserProxyAgent(UserProxyAgent):
 
     def __init__(
         self,
-        name="MathChatAgent", # default set to MathChatAgent
+        name="MathChatAgent",  # default set to MathChatAgent
         system_message="",
         work_dir=None,
-        human_input_mode="NEVER", # Fully automated
+        human_input_mode="NEVER",  # Fully automated
         max_consecutive_auto_reply=None,
         is_termination_msg=None,
         use_docker=True,
@@ -48,11 +121,12 @@ class MathUserProxyAgent(UserProxyAgent):
             **config (dict): other configurations.
         """
         if is_termination_msg is None:
+
             def is_termination_msg(x):
                 cb = extract_code(x)
                 contain_code = False
                 for c in cb:
-                    if c[0] == 'python' or c[0] == 'wolfram':
+                    if c[0] == "python" or c[0] == "wolfram":
                         contain_code = True
                         break
                 return not contain_code and get_answer(x) is not None and get_answer(x) != ""
@@ -70,7 +144,7 @@ class MathUserProxyAgent(UserProxyAgent):
 
         # fixed
         self._max_invalid_q_per_step = max_invalid_q_per_step
-        
+
         # mutable
         self._valid_q_count = 0
         self._total_q_count = 0
@@ -78,7 +152,14 @@ class MathUserProxyAgent(UserProxyAgent):
         self._previous_code = ""
         self.last_reply = None
 
-    def reset(self):
+    def init_chat(self, problem, prompt_type="default", customized_prompt=None):
+        self._reset()
+        if customized_prompt is not None:
+            return customized_prompt + problem
+        else:
+            return PROMPTS[prompt_type] + problem
+
+    def _reset(self):
         super().reset()
         self._valid_q_count = 0
         self._total_q_count = 0
@@ -87,8 +168,7 @@ class MathUserProxyAgent(UserProxyAgent):
         self.last_reply = None
 
     def _add_print_to_last_line(self, s):
-        """ Add print() to the last line of a string.
-        """
+        """Add print() to the last line of a string."""
         # 1. check if there is already a print statement
         if "print(" in s:
             return s
@@ -197,12 +277,12 @@ class MathUserProxyAgent(UserProxyAgent):
                     output, is_success = self._execute_one_python_code(code)
                 elif lang == "wolfram":
                     output, is_success = self._execute_one_wolfram_query(code)
-                
+
                 reply += output + "\n"
                 if not is_success:
                     all_success = False
                     self._valid_q_count -= 1  # count invalid queries
-            
+
             reply = reply.strip()
 
             if self.last_reply == reply:
