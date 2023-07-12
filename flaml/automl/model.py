61730923e055b62eb3e2bee4b284990f5cc7142f
minghao <howt51@gmail.com>
2023-07-12 12:25:44 +0000

    Commenting use_label_encoder - xgboost

--- flaml/automl/model.py
+++ flaml/automl/model.py
@@ -1510,7 +1510,7 @@ class XGBoostEstimator(SKLearnEstimator):
             params["grow_policy"] = params.get("grow_policy", "lossguide")
             params["tree_method"] = params.get("tree_method", "hist")
         # params["booster"] = params.get("booster", "gbtree")
-        params["use_label_encoder"] = params.get("use_label_encoder", False)
+        # params["use_label_encoder"] = params.get("use_label_encoder", False)
         if "n_jobs" in config:
             params["nthread"] = params.pop("n_jobs")
         return params
