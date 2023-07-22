00d1e75883ccde729389b414eb4fdb222124092c
Chi Wang <wang.chi@microsoft.com>
2023-07-22 20:54:53 +0000

    return str not tuple

--- flaml/autogen/agent/math_user_proxy_agent.py
+++ flaml/autogen/agent/math_user_proxy_agent.py
@@ -333,10 +333,7 @@ class MathUserProxyAgent(UserProxyAgent):
         reply = reply.strip()
 
         if self.last_reply == reply:
-            return (
-                reply + "\nYour query or result is same from the last, please try a new approach.",
-                False,
-            )
+            return reply + "\nYour query or result is same from the last, please try a new approach."
         self.last_reply = reply
 
         if not all_success:
