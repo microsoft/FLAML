e2fc33dc31169557c1304a854d6c234baa00b5fe
kevin666aa <yrwu000627@gmail.com>
2023-08-02 11:05:48 -0400

    code format

--- flaml/autogen/agentchat/agent_utils.py
+++ flaml/autogen/agentchat/agent_utils.py
@@ -3,28 +3,25 @@ from typing import List, Union, Dict, Tuple
 import logging
 
 
-
 logger = logging.getLogger(__name__)
-
 
 
 def token_left(messages, model="gpt-3.5-turbo-0613"):
     max_token_limit = {
-        "gpt-3.5-turbo" : 4096,
-        "gpt-3.5-turbo-0301" : 4096,
-        "gpt-3.5-turbo-0613" : 4096,
-        "gpt-3.5-turbo-16k" : 16384,
+        "gpt-3.5-turbo": 4096,
+        "gpt-3.5-turbo-0301": 4096,
+        "gpt-3.5-turbo-0613": 4096,
+        "gpt-3.5-turbo-16k": 16384,
         "gpt-35-turbo": 4096,
-        "gpt-4" : 8192,
+        "gpt-4": 8192,
         "gpt-4-32k": 32768,
         "gpt-4-32k-0314": 32768,  # deprecate in Sep
         "gpt-4-0314": 8192,  # deprecate in Sep
-        "gpt-4-0613" : 8192,
+        "gpt-4-0613": 8192,
         "gpt-4-32k-0613": 32768,
     }
 
     return max_token_limit[model] - num_tokens_from_messages(messages, model=model)
-
 
 
 def num_token_from_text(text: str, model: str = "gpt-3.5-turbo-0613"):
@@ -38,12 +35,12 @@ def num_token_from_text(text: str, model: str = "gpt-3.5-turbo-0613"):
 
 def num_tokens_from_messages(messages: Union[List, Dict], model="gpt-3.5-turbo-0613"):
     """Return the number of tokens used by a list of messages.
-    
+
     retrieved from https://github.com/openai/openai-cookbook/blob/main/examples/How_to_count_tokens_with_tiktoken.ipynb/
     """
     if isinstance(messages, dict):
         messages = [messages]
-        
+
     try:
         encoding = tiktoken.encoding_for_model(model)
     except KeyError:
@@ -56,7 +53,7 @@ def num_tokens_from_messages(messages: Union[List, Dict], model="gpt-3.5-turbo-0
         "gpt-4-32k-0314",
         "gpt-4-0613",
         "gpt-4-32k-0613",
-        }:
+    }:
         tokens_per_message = 3
         tokens_per_name = 1
     elif model == "gpt-3.5-turbo-0301":
