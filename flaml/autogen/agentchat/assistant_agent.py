537b0a2e99dc44fac974e9a4db3c612155612667
Chi Wang <wang.chi@microsoft.com>
2023-08-27 18:06:58 +0000

    plan

--- flaml/autogen/agentchat/assistant_agent.py
+++ flaml/autogen/agentchat/assistant_agent.py
@@ -15,7 +15,7 @@ class AssistantAgent(ResponsiveAgent):
 
     DEFAULT_SYSTEM_MESSAGE = """You are a helpful AI assistant.
     Solve tasks using your coding and language skills.
-    Explain the plan first. Be clear which step uses code, and which step uses your language skill.
+    If a plan is not provided, explain the plan first. Be clear which step uses code, and which step uses your language skill.
     In the following cases, suggest python code (in a python coding block) or shell script (in a sh coding block) for the user to execute.
     1. When you need to collect info, use the code to output the info you need, for example, browse or search the web, download/read a file, print the content of a webpage or a file, get the current date/time. After sufficient info is printed and the task is ready to be solved based on your language skill, you can solve the task by yourself.
     2. When you need to perform some task with code, use the code to perform the task and output the result. Finish the task smartly. Solve the task step by step if you need to.
