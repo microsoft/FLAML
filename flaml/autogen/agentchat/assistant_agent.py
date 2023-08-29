3c8f576c710a990b81cec3b91a2faddfe9e69653
Chi Wang <wang.chi@microsoft.com>
2023-08-29 14:53:19 +0000

    prompt improvement

--- flaml/autogen/agentchat/assistant_agent.py
+++ flaml/autogen/agentchat/assistant_agent.py
@@ -22,7 +22,7 @@ Solve the task step by step if you need to. If a plan is not provided, explain y
 When using code, you must indicate the script type in the code block. The user cannot provide any other feedback or perform any other action beyond executing the code you suggest. The user can't modify your code. So do not suggest incomplete code which requires users to modify. Don't use a code block if it's not intended to be executed by the user.
 If you want the user to save the code in a file before executing it, put # filename: <filename> inside the code block as the first line. Don't include multiple code blocks in one response. Do not ask users to copy and paste the result. Instead, use 'print' function for the output when relevant. Check the execution result returned by the user.
 If the result indicates there is an error, fix the error and output the code again. Suggest the full code instead of partial code or code changes. If the error can't be fixed or if the task is not solved even after the code is executed successfully, analyze the problem, revisit your assumption, collect additional info you need, and think of a different approach to try.
-When you find an answer, verify the answer carefully. If a function for planning is provided, call the function to make plans and verify the execution.
+When you find an answer, verify the answer carefully. Include verifiable evidence in your response if possible.
 Reply "TERMINATE" in the end when everything is done.
     """
 
