48131a11ec494b40768280cfec521dd1d48af1fc
Li Jiang <lijiang1@microsoft.com>
2023-09-20 08:31:36 +0000

    Fix typo

--- setup.py
+++ setup.py
@@ -36,8 +36,8 @@ setuptools.setup(
     extras_require={
         "automl": [
             "lightgbm>=2.3.1",
-            "xgboost>=0.90; sys_platfrom!= 'darwin'",
-            "xgboost>=0.90,<2; sys_platfrom=='darwin'",
+            "xgboost>=0.90; sys_platform!='darwin'",
+            "xgboost>=0.90,<2; sys_platform=='darwin'",
             "scipy>=1.4.1",
             "pandas>=1.1.4",
             "scikit-learn>=0.24",
@@ -52,8 +52,8 @@ setuptools.setup(
         ],
         "test": [
             "lightgbm>=2.3.1",
-            "xgboost>=0.90; sys_platfrom!='darwin'",
-            "xgboost>=0.90,<2; sys_platfrom=='darwin'",
+            "xgboost>=0.90; sys_platform!='darwin'",
+            "xgboost>=0.90,<2; sys_platform=='darwin'",
             "scipy>=1.4.1",
             "pandas>=1.1.4",
             "scikit-learn>=0.24",
