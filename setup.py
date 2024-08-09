31d6d4ec868494241c8712e797985ffa48bae2c4
jirka <jirka.borovec@seznam.cz>
2024-08-09 19:18:00 +0200

    configure pyupgrade with `py3.8+`

--- setup.py
+++ setup.py
@@ -167,5 +167,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.8",
 )
