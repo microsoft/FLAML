5e32c5690ac37f3f6983031905e2b26326cbfd64
Li Jiang <bnujli@gmail.com>
2024-08-11 17:40:49 +0800

    Add warning message for not has_automl

--- flaml/__init__.py
+++ flaml/__init__.py
@@ -2,8 +2,10 @@ import logging
 
 try:
     from flaml.automl import AutoML, logger_formatter
+
+    has_automl = True
 except ImportError:
-    pass
+    has_automl = False
 from flaml.onlineml.autovw import AutoVW
 from flaml.tune.searcher import CFO, FLOW2, BlendSearch, BlendSearchTuner, RandomSearch
 from flaml.version import __version__
@@ -11,3 +13,6 @@ from flaml.version import __version__
 # Set the root logger.
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
+
+if not has_automl:
+    logger.warning("flaml.automl is not available. Please install flaml[automl] to enable AutoML functionalities.")
