ff86d03e97898f1b5a7444fc957bf1df6aef9563
Li Jiang <lijiang1@microsoft.com>
2024-08-12 08:36:31 +0000

    Update parent run

--- test/automl/test_mlflow.py
+++ test/automl/test_mlflow.py
@@ -12,7 +12,7 @@ from flaml import AutoML
 class TestMLFlowLoggingParam:
     def test_should_start_new_run_by_default(self, automl_settings):
         with mlflow.start_run() as parent_run:
-            parent = mlflow.last_active_run()
+            mlflow.last_active_run()
             automl = AutoML()
             X_train, y_train = load_iris(return_X_y=True)
             automl.fit(X_train=X_train, y_train=y_train, **automl_settings)
@@ -21,12 +21,12 @@ class TestMLFlowLoggingParam:
             except FileNotFoundError:
                 print("[WARNING]: No file found")
 
-        children = self._get_child_runs(parent)
+        children = self._get_child_runs(parent_run)
         assert len(children) >= 1, f"Expected at least 1 child run, got {len(children)}"
 
     def test_should_not_start_new_run_when_mlflow_logging_set_to_false_in_init(self, automl_settings):
         with mlflow.start_run() as parent_run:
-            parent = mlflow.last_active_run()
+            mlflow.last_active_run()
             automl = AutoML(mlflow_logging=False)
             X_train, y_train = load_iris(return_X_y=True)
             automl.fit(X_train=X_train, y_train=y_train, **automl_settings)
@@ -35,12 +35,12 @@ class TestMLFlowLoggingParam:
             except FileNotFoundError:
                 print("[WARNING]: No file found")
 
-        children = self._get_child_runs(parent)
+        children = self._get_child_runs(parent_run)
         assert len(children) == 0, f"Expected 0 child runs, got {len(children)}"
 
     def test_should_not_start_new_run_when_mlflow_logging_set_to_false_in_fit(self, automl_settings):
         with mlflow.start_run() as parent_run:
-            parent = mlflow.last_active_run()
+            mlflow.last_active_run()
             automl = AutoML()
             X_train, y_train = load_iris(return_X_y=True)
             automl.fit(X_train=X_train, y_train=y_train, mlflow_logging=False, **automl_settings)
@@ -49,12 +49,12 @@ class TestMLFlowLoggingParam:
             except FileNotFoundError:
                 print("[WARNING]: No file found")
 
-        children = self._get_child_runs(parent)
+        children = self._get_child_runs(parent_run)
         assert len(children) == 0, f"Expected 0 child runs, got {len(children)}"
 
     def test_should_start_new_run_when_mlflow_logging_set_to_true_in_fit(self, automl_settings):
         with mlflow.start_run() as parent_run:
-            parent = mlflow.last_active_run()
+            mlflow.last_active_run()
             automl = AutoML(mlflow_logging=False)
             X_train, y_train = load_iris(return_X_y=True)
             automl.fit(X_train=X_train, y_train=y_train, mlflow_logging=True, **automl_settings)
@@ -63,7 +63,7 @@ class TestMLFlowLoggingParam:
             except FileNotFoundError:
                 print("[WARNING]: No file found")
 
-        children = self._get_child_runs(parent)
+        children = self._get_child_runs(parent_run)
         assert len(children) >= 1, f"Expected at least 1 child run, got {len(children)}"
 
     @staticmethod
@@ -91,6 +91,7 @@ class TestMLFlowLoggingParam:
 
     @pytest.fixture(scope="class")
     def automl_settings(self):
+        mlflow.end_run()
         return {
             "time_budget": 15,  # in seconds
             "metric": "accuracy",
