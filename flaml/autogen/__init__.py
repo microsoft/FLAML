65f12a8d2d216d3cc94834a926a26d85d66ecd03
Li Jiang <lijiang1@microsoft.com>
2026-01-09 07:06:41 +0000

    Remove autogen

--- flaml/autogen/__init__.py
+++ flaml/autogen/__init__.py
@@ -6,7 +6,7 @@ from .oai import *
 
 warnings.warn(
     "The `flaml.autogen` module is deprecated and will be removed in a future release. "
-    "Please refer to `https://github.com/microsoft/autogen` instead.",
+    "Please refer to `https://github.com/microsoft/autogen` for latest usage.",
     DeprecationWarning,
     stacklevel=2,
 )
