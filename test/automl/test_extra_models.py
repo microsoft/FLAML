b16c1b5a273a41d577465576eae331eeae5454e4
Li Jiang <lijiang1@microsoft.com>
2026-01-19 08:22:50 +0000

    Fix low is out of bounds for int32

--- test/automl/test_extra_models.py
+++ test/automl/test_extra_models.py
@@ -192,16 +192,7 @@ def _test_sparse_matrix_classification(estimator):
     # Windows + Python 3.13), `scipy.sparse.random(..., dtype=int)` may trigger
     # integer sampling paths which raise "low is out of bounds for int32".
     # A float sparse matrix is sufficient to validate sparse-input support.
-    rs = np.random.RandomState(0)
-    X_train = scipy.sparse.random(
-        1554,
-        21,
-        density=0.1,
-        format="csr",
-        random_state=rs,
-        data_rvs=rs.random_sample,
-        dtype=np.float32,
-    )
+    X_train = scipy.sparse.random(1554, 21, dtype=np.float32)
     y_train = np.random.randint(3, size=1554)
     automl_experiment.fit(X_train=X_train, y_train=y_train, **automl_settings)
 
