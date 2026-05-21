c8f4763e540f18169fdef3c9218a3dad7839e2fb
Imran Ahamed <immu4989@gmail.com>
2026-05-21 10:30:28 -0500

    fix: LRL2Classifier honors random_seed config (#1552)

--- flaml/automl/model.py
+++ flaml/automl/model.py
@@ -2068,6 +2068,9 @@ class LRL2Classifier(SKLearnEstimator):
 
     def __init__(self, task="binary", **config):
         super().__init__(task, **config)
+        random_seed = self.params.pop("random_seed", config.get("random_seed", 10242048))
+        if "random_state" not in self.params:
+            self.params["random_state"] = random_seed
         assert self._task.is_classification(), "LogisticRegression for classification task only"
         self.estimator_class = LogisticRegression
 
